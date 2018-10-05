e8b0539facd8cba1224d3afc08133edd920cd112
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 16:26:56 -0500

    Create 69sqrt(x).py

---
+++ 69sqrt(x).py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 0, x // 2 + 1
+        while left + 1 < right:
+            middle = left + (right - left) // 2
+            if middle**2 < x: left = middle
+            else: right = middle
+        if right**2 == x : return right
+        else: return left
