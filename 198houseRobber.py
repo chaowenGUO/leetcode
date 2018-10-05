f14435a87bcf316dab2b7992dfe5aacd114d8aca
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 16:44:13 -0500

    Create 198houseRobber.py

---
+++ 198houseRobber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = b = 0
+        for num in nums: a, b = b, max(a + num, b)
+        return b
