834e20b64e06e753b3c5430f16f28906ca882ccc
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 16:47:08 -0500

    Create 226invertBinaryTree.py

---
+++ 226invertBinaryTree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root: root.left, root.right = map(self.invertTree, (root.right, root.left))
+        return root
