15282b087c86375e40c9ebb1ef89eb740aa80305
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 16:03:36 -0500

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictionary = {}
+        for index, num in enumerate(nums):
+            if num in dictionary: return [index, dictionary.get(num)]
+            else: dictionary[target - num] = index
