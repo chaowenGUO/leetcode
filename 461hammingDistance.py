4efcf84b1a207ef509d5ef0612fc7a1775e0a9cb
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 17:01:39 -0500

    Create 461hammingDistance.py

---
+++ 461hammingDistance.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        n = x ^ y
+        result = 0
+        while n:
+            n = n & n - 1
+            result += 1
+        return result
