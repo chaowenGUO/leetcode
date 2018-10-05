f10d057f5e11aaf215e6eb0e651da8591beb3ed9
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 17:13:12 -0500

    Create 118pascal'sTriangle.py

---
+++ 118pascal'sTriangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = [[1]]
+        for _ in range(numRows - 1): result += [[sum(_) for _ in zip([0] + result[-1], result[-1] + [0])]]
+        return result[:numRows]
