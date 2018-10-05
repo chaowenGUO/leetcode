212a4909b8ec2c524a27fa06bd01670e62eaca54
chaowen guo <chaowen.guo1@gmail.com>
2018-10-05 17:01:04 -0500

    Create 429n-aryTreeLevelOrderTraversal.py

---
+++ 429n-aryTreeLevelOrderTraversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        result, level = [], [root]
+        while root and level:
+            result += [[node.val for node in level]]
+            level = [child for node in level for child in node.children if child]
+        return result
