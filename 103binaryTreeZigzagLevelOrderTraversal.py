26f5d5abd1ca4224e5697d1be7920facc95c0b87
chaowen guo <chaowen.guo1@gmail.com>
2018-10-08 18:52:02 -0500

    Update 103binaryTreeZigzagLevelOrderTraversal.py

--- 103binaryTreeZigzagLevelOrderTraversal.py
+++ 103binaryTreeZigzagLevelOrderTraversal.py
@@ -13,7 +13,7 @@ class Solution:
         """
         result, level, direction = [], [root], 1
         while root and level:
-            result += [[node.val for node in level][::direction]]
+            result += [node.val for node in level][::direction],
             direction *= -1
             level = [child for node in level for child in (node.left, node.right) if child]
         return result
