ea9775b3cf70104a8b903dc6261fceb01765422d
chaowen guo <chaowen.guo1@gmail.com>
2018-10-08 17:52:43 -0500

    Update 345reverseVowelsOfAString.py

--- 345reverseVowelsOfAString.py
+++ 345reverseVowelsOfAString.py
@@ -4,6 +4,14 @@ class Solution:
         :type s: str
         :rtype: str
         """
-        import re
-        vowels = re.findall('[aeiouAEIOU]', s)
-        return re.sub('[aeiouAEIOU]', lambda _: vowels.pop(), s)
+        s = [*s]
+        left, right = 0, len(s) - 1
+        vowels = 'aeiouAEIOU'
+        while left < right:
+            if s[left] not in vowels: left += 1
+            elif s[right] not in vowels: right -= 1
+            else:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+        return ''.join(s)
