552eb61af2d7cbed05df6619c6ddd0412a11e52f
chaowen guo <chaowen.guo1@gmail.com>
2018-10-08 22:06:01 -0500

    Create 137singleNumberII.py

---
+++ 137singleNumberII.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        a = b = 0
+        for num in nums:
+            a = (a ^ num) & b
+            b = (b ^ num) | a
+        return b
