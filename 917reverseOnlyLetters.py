b081a26f6406637f00db79b3815d69e120724981
chaowen guo <chaowen.guo1@gmail.com>
2018-10-12 19:10:23 -0500

    Create 917reverseOnlyLetters.py

---
+++ 917reverseOnlyLetters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        S = [*S]
+        left, right = 0, len(S) - 1
+        while left < right:
+            if not S[left].isalpha(): left += 1
+            elif not S[right].isalpha(): right -= 1
+            else:
+                S[left], S[right] = S[right], S[left]
+                left += 1
+                right -= 1
+        return ''.join(S)
