4254ddaa1da2bdc146cf4c49f2ad004a945cd5d1
chaowen guo <chaowen.guo1@gmail.com>
2018-10-12 23:38:37 -0500

    Create 242validAnagram.py

---
+++ 242validAnagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        import collections
+        return collections.Counter(s) == collections.Counter(t)
