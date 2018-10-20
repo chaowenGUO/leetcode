6e5166f53c53858704e2903e6f9d5dec848ba39b
chaowen guo <chaowen.guo1@gmail.com>
2018-10-19 23:01:30 -0500

    Create 347topKFrequentElements.py

---
+++ 347topKFrequentElements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        import collections
+        return [num[0] for num in collections.Counter(nums).most_common(k)]
