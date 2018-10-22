a87ad60790ade193076a475234bdde5659d591bb
chaowen guo <chaowen.guo1@gmail.com>
2018-10-22 03:41:16 -0500

    Create 162findPeakElement.py

---
+++ 162findPeakElement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left + 1 < right:
+            middle = left + (right - left) // 2
+            if nums[middle] < nums[middle + 1]: left = middle
+            else: right = middle
+        return left if nums[left] > nums[right] else right
