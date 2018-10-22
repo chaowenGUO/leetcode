55cf41ace2567f6a6d4469d0830686a9409f8180
chaowen guo <chaowen.guo1@gmail.com>
2018-10-22 03:02:51 -0500

    Update 905sortArrayByParity.py

--- 905sortArrayByParity.py
+++ 905sortArrayByParity.py
@@ -6,7 +6,7 @@ class Solution:
         """
         left, right = 0, len(A) - 1
         while left < right:
-            while A[left] & 1 == 0 and left < right: left += 1
-            while A[right] & 1 == 1 and left < right: right -= 1
-            A[left], A[right] = A[right], A[left]
+            if A[left] & 1 == 0: left += 1
+            elif A[right] & 1 == 1: right -= 1
+            else: A[left], A[right] = A[right], A[left]
         return A
