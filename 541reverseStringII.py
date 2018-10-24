8543635ad5ed173fc34f539d57e2b32692b57204
chaowen guo <chaowen.guo1@gmail.com>
2018-10-23 21:02:17 -0500

    Update 541reverseStringII.py

--- 541reverseStringII.py
+++ 541reverseStringII.py
@@ -5,4 +5,6 @@ class Solution:
         :type k: int
         :rtype: str
         """
-        return s[:k][::-1] + s[k:2 * k] + self.reverseStr(s[2 * k:], k) if s else ''
+        s = [*s]
+        for i in range(0, len(s), 2 * k): s[i:i + k] = reversed(s[i:i + k])
+        return ''.join(s)
