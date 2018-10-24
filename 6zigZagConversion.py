9e07d25ed528c82f46378986955eb4f6cbdc93a8
chaowen guo <chaowen.guo1@gmail.com>
2018-10-23 21:05:00 -0500

    Create 6zigZagConversion.py

---
+++ 6zigZagConversion.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s): return s
+        result = [''] * numRows
+        row, direction = 0, 1
+        for char in s:
+            result[row] += char 
+            if row == 0: direction = 1
+            elif row == numRows - 1: direction = -1
+            row += direction
+        return ''.join(result)
