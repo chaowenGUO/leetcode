c8b82d5f53dc9d8e7145b52e617dd1d96459d212
chaowen guo <chaowen.guo1@gmail.com>
2018-10-23 20:50:43 -0500

    Update 415addStrings.py

--- 415addStrings.py
+++ 415addStrings.py
@@ -8,7 +8,7 @@ class Solution:
         import itertools
         carry = 0
         result = ''
-        for _ in itertools.zip_longest(num1[::-1], num2[::-1], fillvalue = '0'):
+        for _ in itertools.zip_longest(reversed(num1), reversed(num2), fillvalue = '0'):
             carry += sum(map(int, _))
             result = str(carry % 10) + result
             carry //= 10
