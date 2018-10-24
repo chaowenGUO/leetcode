52417c94ba9d0995aa69d1ee3e39c5b19261a282
chaowen guo <chaowen.guo1@gmail.com>
2018-10-24 00:21:28 -0500

    Update 559maximumDepthOfN-aryTree.py

--- 559maximumDepthOfN-aryTree.py
+++ 559maximumDepthOfN-aryTree.py
@@ -11,4 +11,5 @@ class Solution:
         :type root: Node
         :rtype: int
         """
-        return max([self.maxDepth(_) for _ in root.children] + [0]) + 1 if root else 0
+        import itertools
+        return max(itertools.chain(map(self.maxDepth, root.children), [0])) + 1 if root else 0
