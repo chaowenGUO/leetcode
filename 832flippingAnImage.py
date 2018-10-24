3e019e3f65edb0830d268bca4517b1190e8b9445
chaowen guo <chaowen.guo1@gmail.com>
2018-10-24 01:12:59 -0500

    Create 832flippingAnImage.py

---
+++ 832flippingAnImage.py
@@ -0,0 +1,7 @@
+class Solution:
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[_^ 1 for _ in reversed(row)] for row in A]
