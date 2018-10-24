c723a64c7021d2f710c4d7d16b74c1f34169328c
chaowen guo <chaowen.guo1@gmail.com>
2018-10-23 20:52:43 -0500

    Update 54spiralMatrix.py

--- 54spiralMatrix.py
+++ 54spiralMatrix.py
@@ -4,4 +4,4 @@ class Solution:
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+        return matrix and matrix.pop(0) + self.spiralOrder([*map(list,zip(*matrix))][::-1])
