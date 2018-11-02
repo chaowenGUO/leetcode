a6b7c34918ff0008604b9614cb3a9f98602321d5
chaowen guo <chaowen.guo1@gmail.com>
2018-11-02 03:37:26 -0500

    Update 6zigZagConversion.py

--- 6zigZagConversion.py
+++ 6zigZagConversion.py
@@ -6,11 +6,12 @@ class Solution:
         :rtype: str
         """
         if numRows == 1: return s
-        result = [''] * numRows
-        row, direction = 0, 1
-        for char in s:
-            result[row] += char 
-            if row == 0: direction = 1
-            elif row == numRows - 1: direction = -1
-            row += direction
-        return ''.join(result)
+        else:
+            result = [' '] * numRows
+            row, direction = 0, 1
+            for char in s:
+                result[row] += char
+                if row == 0: direction = 1
+                elif row == numRows - 1: direction = -1
+                row += direction
+            return ''.join(result)
