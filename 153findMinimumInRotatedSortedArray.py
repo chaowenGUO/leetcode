93c418e56571e64661c9ca4457c25c0c32bc56a3
chaowen guo <chaowen.guo1@gmail.com>
2018-11-02 03:36:19 -0500

    Update 153findMinimumInRotatedSortedArray.py

--- 153findMinimumInRotatedSortedArray.py
+++ 153findMinimumInRotatedSortedArray.py
@@ -6,8 +6,9 @@ class Solution:
         """
         left, right = 0, len(nums) - 1
         if nums[left] < nums[right]: return nums[left]
-        while left + 1 < right:
-            middle = left + (right - left) // 2
-            if nums[left] < nums[middle]: left = middle
-            else: right = middle
-        return min(nums[left], nums[right])
+        else:
+            while left + 1 < right:
+                middle = left + (right - left) // 2
+                if nums[left] < nums[middle]: left = middle
+                else: right = middle
+            return min(nums[left], nums[right])
