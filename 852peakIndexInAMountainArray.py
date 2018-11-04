a7918ee7a8f998303d0a9a0d571992d360dad58f
chaowen guo <chaowen.guo1@gmail.com>
2018-11-04 14:18:16 -0600

    Create 852peakIndexInAMountainArray.py

---
+++ 852peakIndexInAMountainArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(A) - 1
+        while left + 1 < right:
+            middle = left + (right - left) // 2
+            if A[middle] < A[middle + 1]: left = middle
+            else: right = middle
+        return left if A[left] > A[right] else right
