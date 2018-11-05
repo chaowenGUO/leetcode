a7a945172b4118cbeaa8bfb1a43e389754d23719
chaowen guo <chaowen.guo1@gmail.com>
2018-11-04 19:26:29 -0600

    Create 633sumOfSquareNumbers.py

---
+++ 633sumOfSquareNumbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        import math
+        left, right = 0, int(math.sqrt(c))
+        while left <= right:
+            square = left**2 + right**2
+            if square == c: return True
+            elif square < c: left += 1
+            else: right -= 1
+        return False
