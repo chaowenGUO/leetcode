4010cd1b9522648e47cc6adae7cecafd6e42ea66
chaowen guo <chaowen.guo1@gmail.com>
2018-11-06 13:14:03 -0600

    Update 397integerReplacement.py

--- 397integerReplacement.py
+++ 397integerReplacement.py
@@ -6,7 +6,7 @@ class Solution:
         """
         result = 0
         while n > 1:
-            if n & 1 == 0: n //= 2
+            if n & 1 == 0: n >>= 1
             elif n & 3 == 1 or n == 3: n -= 1
             else: n += 1
             result += 1
