6f18b0add8ea77eb3622f57c19466aa5232c9872
chaowen guo <chaowen.guo1@gmail.com>
2018-11-15 22:47:49 -0600

    Create 257binaryTreePaths.py

---
+++ 257binaryTreePaths.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root: return []
+        elif not root.left and not root.right: return [str(root.val)]
+        else: return [str(root.val) + '->' + _ for child in (root.left, root.right) for _ in self.binaryTreePaths(child) if child]
