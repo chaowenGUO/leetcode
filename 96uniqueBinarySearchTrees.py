8240b9bbdd1ff31beb5425c2d5a7bd1e720a87f0
chaowen guo <chaowen.guo1@gmail.com>
2018-11-23 03:45:10 -0600

    Update 96uniqueBinarySearchTrees.py

--- 96uniqueBinarySearchTrees.py
+++ 96uniqueBinarySearchTrees.py
@@ -4,6 +4,5 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        catalan = 1
-        for _ in range(n): catalan = catalan * 2 * (2 * _ + 1) // (_ + 2)
-        return catalan
+        import functools
+        return functools.reduce(lambda catalan, _: catalan * 2 * (2 * _ + 1) // (_ + 2), range(n), 1)
