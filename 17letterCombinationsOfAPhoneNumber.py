6fe1825969e1b1cc69b0b822932e2fe5619e8b0f
chaowen guo <chaowen.guo1@gmail.com>
2018-11-26 23:39:53 -0600

    Update 17letterCombinationsOfAPhoneNumber.py

--- 17letterCombinationsOfAPhoneNumber.py
+++ 17letterCombinationsOfAPhoneNumber.py
@@ -4,8 +4,6 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
-        import itertools
+        import functools, itertools
         dictionary = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
-        result = ['']
-        for digit in digits: result = [''.join(_) for _ in itertools.product(result, dictionary.get(digit))]
-        return result if digits else []
+        return functools.reduce(lambda result, digit: [''.join(_) for _ in itertools.product(result, dictionary.get(digit))], digits, ['']) if digits else []
