ff75f6d58292f5b67ae3430ed249a1ac0c291e29
chaowen guo <chaowen.guo1@gmail.com>
2019-01-07 19:48:57 -0600

    Create 12integerToRoman.py

---
+++ 12integerToRoman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        result = ''
+        for symbol, value in zip(symbols, values):
+            result += symbol * int(num // value)
+            num %= value
+        return result
