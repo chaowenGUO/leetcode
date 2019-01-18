80df380022b97520531030e56b6c1e6e55c3974b
chaowen guo <chaowen.guo1@gmail.com>
2019-01-17 21:41:47 -0600

    Update 37sudokuSolver.py

--- 37sudokuSolver.py
+++ 37sudokuSolver.py
@@ -1,11 +1,12 @@
 class Solution:
+    import string
     def dfs(self, board, stack):
         if not stack: return
         x, y = stack.pop()
         box = [board[x // 3 * 3 + i][y // 3 * 3 + j] for i in range(3) for j in range(3)]
         row = [board[x][_] for _ in range(9)]
         column = [board[_][y] for _ in range(9)]
-        for _ in '123456789':
+        for _ in string.digits[1:]:
             if not any([_ in box, _ in row, _ in column]):
                 board[x][y] = _
                 self.dfs(board, stack)
