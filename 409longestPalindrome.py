abac96aebab1a8b512b428b78bb1ca39f1a65a73
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 05:46:58 -0600

    Update 409longestPalindrome.py

--- 409longestPalindrome.py
+++ 409longestPalindrome.py
@@ -1,9 +1,5 @@
 class Solution:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def longestPalindrome(self, s: str) -> int:
         import collections
         odd = sum(_ & 1 for _ in collections.Counter(s).values())
         return len(s) - odd + bool(odd)
