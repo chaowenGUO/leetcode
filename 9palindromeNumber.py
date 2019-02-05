b90c549c26427c744f6d1711e04f96b005c87172
chaowen guo <chaowen.guo1@gmail.com>
2019-02-04 23:26:03 -0600

    Update 9palindromeNumber.py

--- 9palindromeNumber.py
+++ 9palindromeNumber.py
@@ -1,9 +1,5 @@
 class Solution:
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+    def isPalindrome(self, x: int) -> bool:
         if x < 0 or x % 10 == 0 and x != 0: return False
         else:
             result = 0
