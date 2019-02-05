5e98e492c6307e56ca1d206a53c60be42aab64b0
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 06:44:27 -0600

    Update 657robotReturnToOrigin.py

--- 657robotReturnToOrigin.py
+++ 657robotReturnToOrigin.py
@@ -1,9 +1,5 @@
 class Solution:
-    def judgeCircle(self, moves):
-        """
-        :type moves: str
-        :rtype: bool
-        """
+    def judgeCircle(self, moves: str) -> bool:
         import collections
         dictionary = collections.Counter(moves)
-        return dictionary.get('U') == dictionary.get('D') and dictionary.get('L') == dictionary.get('R')
+        return dictionary.get('L') == dictionary.get('R') and dictionary.get('U') == dictionary.get('D')
