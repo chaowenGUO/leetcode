2dc771aafd26619b5242539afa4d9a36ac4750b8
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 00:24:40 -0600

    Update 42trappingRainWater.py

--- 42trappingRainWater.py
+++ 42trappingRainWater.py
@@ -1,9 +1,6 @@
+import typing
 class Solution:
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+    def trap(self, height: typing.List[int]) -> int:
         left, right = 0, len(height) - 1
         result = leftMax = rightMax = 0
         while left < right:
