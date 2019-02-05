d32e61e47c1f54b9fc098c28193494496b00378f
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 02:05:59 -0600

    Update 70climbingStairs.py

--- 70climbingStairs.py
+++ 70climbingStairs.py
@@ -1,8 +1,4 @@
 class Solution:
-    def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def climbStairs(self, n: int) -> int:
         import functools
         return functools.reduce(lambda result, _: [result[1], sum(result)], range(n), [1] * 2)[0]
