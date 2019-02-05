c764a6254d95e384b1c080ff5c0193f39bf96f6e
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:00:00 -0600

    Update 796rotateString.py

--- 796rotateString.py
+++ 796rotateString.py
@@ -1,8 +1,3 @@
 class Solution:
-    def rotateString(self, A, B):
-        """
-        :type A: str
-        :type B: str
-        :rtype: bool
-        """
+    def rotateString(self, A: str, B: str) -> bool:
         return len(A) == len(B) and B in A * 2
