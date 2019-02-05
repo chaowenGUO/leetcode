93087a143badff53779e3dcb5114631db9adf4ef
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 01:55:50 -0600

    Update 65validNumber.py

--- 65validNumber.py
+++ 65validNumber.py
@@ -1,8 +1,4 @@
 class Solution:
-    def isNumber(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+    def isNumber(self, s: str) -> bool:
         import re
         return bool(re.search('^[+-]?(\.\d+|\d+\.?\d*)(e[+-]?\d+)?$', s.strip()))
