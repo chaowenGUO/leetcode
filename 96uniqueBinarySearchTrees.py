77b5c2cc68ab8ab0af8faf0623e68007757bdbc8
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 02:36:47 -0600

    Update 96uniqueBinarySearchTrees.py

--- 96uniqueBinarySearchTrees.py
+++ 96uniqueBinarySearchTrees.py
@@ -1,8 +1,4 @@
 class Solution:
-    def numTrees(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def numTrees(self, n: int) -> int:
         import functools
         return functools.reduce(lambda catalan, _: catalan * 2 * (2 * _ + 1) // (_ + 2), range(n), 1)
