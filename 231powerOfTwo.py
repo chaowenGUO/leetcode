81713ca6fce0cd91ccf82b9e563f7228b173fa04
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 04:00:37 -0600

    Update 231powerOfTwo.py

--- 231powerOfTwo.py
+++ 231powerOfTwo.py
@@ -1,7 +1,3 @@
 class Solution:
-    def isPowerOfTwo(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
+    def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and not n & n - 1
