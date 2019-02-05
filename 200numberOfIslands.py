3ff1b1f814d382a23cd7dd0ee12587f83a60217b
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 03:26:32 -0600

    Update 200numberOfIslands.py

--- 200numberOfIslands.py
+++ 200numberOfIslands.py
@@ -1,17 +1,14 @@
+import typing
 class Solution:
-    def sink(self, grid, row, column):
-        if 0 <= row < len(grid) and  0 <= column < len(grid[0]) and grid[row][column] == '1':
+    def sink(self, grid: typing.List[typing.List[str]], row: int, column: int) -> None:
+        if 0 <= row < len(grid) and 0 <= column < len(grid[0]) and grid[row][column] == '1':
             grid[row][column] = '0'
             self.sink(grid, row + 1, column)
             self.sink(grid, row - 1, column)
             self.sink(grid, row, column + 1)
             self.sink(grid, row, column - 1)
         
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+    def numIslands(self, grid: typing.List[typing.List[str]]) -> int:
         result = 0
         for row in range(len(grid)):
             for column in range(len(grid[0])):
