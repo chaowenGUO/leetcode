b95f98e98e9785554457efc0a130a9a1800758ba
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 04:49:29 -0600

    Update 46permutations.py

--- 46permutations.py
+++ 46permutations.py
@@ -1,8 +1,5 @@
+import typing
 class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+    def permute(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
         import functools
-        return functools.reduce(lambda result, num: [permutation[_:] + [num] + permutation[:_] for permutation in result for _ in range(len(permutation) + 1)], nums, [[]])
+        return functools.reduce(lambda result, num: [permutation[:_] + [num] + permutation[_:] for permutation in result for _ in range(len(permutation) + 1)], nums, [[]])
