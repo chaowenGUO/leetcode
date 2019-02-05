7ac950fb59cb3e4f6453b2159fa00d9c927e58ed
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 00:58:44 -0600

    Update 56mergeIntervals.py

--- 56mergeIntervals.py
+++ 56mergeIntervals.py
@@ -4,14 +4,11 @@
 #         self.start = s
 #         self.end = e
 
+import typing
 class Solution:
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
+    def merge(self, intervals: typing.List[Interval]) -> typing.List[Interval]:
         result = []
-        for interval in sorted(intervals, key = lambda interval: interval.start):
+        for interval in sorted(intervals, key = lambda _:_.start):
             if result and interval.start <= result[-1].end: result[-1].end = max(result[-1].end, interval.end)
             else: result += interval,
         return result
