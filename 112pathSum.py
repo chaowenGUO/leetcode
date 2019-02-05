7a182c6d658d7e5b69ea1ce6eaa25be594d94161
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:33:33 -0600

    Update 112pathSum.py

--- 112pathSum.py
+++ 112pathSum.py
@@ -6,12 +6,7 @@
 #         self.right = None
 
 class Solution:
-    def hasPathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: bool
-        """
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root: return False
         elif not root.left and not root.right: return sum == root.val
         else: return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
