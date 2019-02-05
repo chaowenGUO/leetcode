15cbbf1cb6b8aa4d4ee9a7a4ebb34ea868519af2
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:27:43 -0600

    Update 136singleNumber.py

--- 136singleNumber.py
+++ 136singleNumber.py
@@ -1,8 +1,5 @@
+import typing
 class Solution:
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def singleNumber(self, nums: typing.List[int]) -> int:
         import functools, operator
         return functools.reduce(operator.xor, nums)
