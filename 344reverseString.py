6930e4f374ae7e262bcd835a5a0a61164021da45
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 05:05:53 -0600

    Update 344reverseString.py

--- 344reverseString.py
+++ 344reverseString.py
@@ -1,13 +1,11 @@
+import typing
 class Solution:
-    def reverseString(self, s):
+    def reverseString(self, s: typing.List[str]) -> None:
         """
-        :type s: str
-        :rtype: str
+        Do not return anything, modify s in-place instead.
         """
-        s = [*s]
         left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
-        return ''.join(s)
