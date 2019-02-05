60e80d46bcd4dee2029c3df3ecd637377bc8c203
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 08:35:25 -0600

    Update 179largestNumber.py

--- 179largestNumber.py
+++ 179largestNumber.py
@@ -1,7 +1,4 @@
+import typing
 class Solution:
-    def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
-        return str(int(''.join(sorted(map(str, nums), key = lambda _: _ * 9, reverse = True))
+    def largestNumber(self, nums: typing.List[int]) -> str:
+        return str(int(''.join(sorted(map(str, nums), key = lambda _: _ * 9, reverse = True))))
