b8be230b204b1753e0bd075929fe5d3827620a94
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 04:19:18 -0600

    Update 264uglyNumberII.py

--- 264uglyNumberII.py
+++ 264uglyNumberII.py
@@ -1,8 +1,4 @@
 class Solution:
     ugly = sorted(2**a * 3**b * 5**c for a in range(32) for b in range(20) for c in range(14))
-    def nthUglyNumber(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def nthUglyNumber(self, n: int) -> int:
         return self.ugly[n - 1]
