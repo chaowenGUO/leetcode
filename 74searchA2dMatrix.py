d1141ef8d26ddb054fae0c47de7ae5f1a489144f
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 02:19:38 -0600

    Update 74searchA2dMatrix.py

--- 74searchA2dMatrix.py
+++ 74searchA2dMatrix.py
@@ -1,15 +1,12 @@
+import typing
 class Solution:
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
+    def searchMatrix(self, matrix: typing.List[typing.List[int]], target: int) -> bool:
         if not matrix or not matrix[0]: return False
-        column = len(matrix[0])
-        left, right = 0, len(matrix) * column - 1
-        while left + 1 < right:
-            middle = left + (right - left) // 2
-            if matrix[middle // column][middle % column] < target: left = middle
-            else: right = middle
-        return True if matrix[left // column][left % column] == target or matrix[right // column][right % column] == target else False
+        else:
+            column = len(matrix[0])
+            left, right = 0, len(matrix) * column - 1
+            while left + 1 < right:
+                middle = left + (right - left) // 2
+                if matrix[middle // column][middle % column] < target: left = middle
+                else: right = middle
+            return True if matrix[left // column][left % column] == target or matrix[right // column][right % column] == target else False
