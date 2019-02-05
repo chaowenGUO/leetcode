3568f538128ddc60d540f8baec283b1c30e2f8d3
chaowen guo <chaowen.guo1@gmail.com>
2019-02-04 23:41:37 -0600

    Update 17letterCombinationsOfAPhoneNumber.py

--- 17letterCombinationsOfAPhoneNumber.py
+++ 17letterCombinationsOfAPhoneNumber.py
@@ -1,9 +1,6 @@
+import typing
 class Solution:
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+    def letterCombinations(self, digits: str) -> typing.List[str]:
         import functools, itertools
         dictionary = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
         return functools.reduce(lambda result, digit: [''.join(_) for _ in itertools.product(result, dictionary.get(digit))], digits, ['']) if digits else []
