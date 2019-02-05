a4c2a7af4d8cf98c5cf6605bd4955d72fbd5e2e1
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 04:38:04 -0600

    Update 2addTwoNumbers.py

--- 2addTwoNumbers.py
+++ 2addTwoNumbers.py
@@ -5,12 +5,7 @@
 #         self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         result = current = ListNode(None)
         carry = 0
         while l1 or l2 or carry:
@@ -21,6 +16,7 @@ class Solution:
                 carry += l2.val
                 l2 = l2.next
             current.next = ListNode(carry % 10)
-            current = current.next
             carry //= 10
+            current = current.next
+        return result.next
         return result.next
