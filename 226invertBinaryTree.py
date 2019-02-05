03635421f2980dad8703f0dc918b57bf51066536
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 03:52:25 -0600

    Update 226invertBinaryTree.py

--- 226invertBinaryTree.py
+++ 226invertBinaryTree.py
@@ -6,10 +6,6 @@
 #         self.right = None
 
 class Solution:
-    def invertTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
+    def invertTree(self, root: TreeNode) -> TreeNode:
         if root: root.left, root.right = map(self.invertTree, (root.right, root.left))
         return root
