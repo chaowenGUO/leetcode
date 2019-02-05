034e5f6de2ca831d346ae9bc3cdc6e8a1b359f53
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 05:50:05 -0600

    Update 412fizzBuzz.py

--- 412fizzBuzz.py
+++ 412fizzBuzz.py
@@ -1,7 +1,4 @@
+import typing
 class Solution:
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
+    def fizzBuzz(self, n: int) -> typing.List[str]:
         return ['Fizz' * (not _ % 3) + 'Buzz' * (not _ % 5) or str(_) for _ in range(1, n + 1)]
