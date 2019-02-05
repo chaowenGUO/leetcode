474ff747262e91abb853f2b2214d86c35a8c1ae6
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 08:10:43 -0600

    Update 19removeNthNodeFromEndOfList.py

--- 19removeNthNodeFromEndOfList.py
+++ 19removeNthNodeFromEndOfList.py
@@ -5,17 +5,13 @@
 #         self.next = None
 
 class Solution:
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         fast = slow = head
         for _ in range(n): fast = fast.next
         if not fast: return head.next
-        while fast.next:
-            fast = fast.next
-            slow = slow.next
-        slow.next = slow.next.next
-        return head
+        else:
+            while fast.next:
+                fast = fast.next
+                slow = slow.next
+            slow.next = slow.next.next
+            return head
