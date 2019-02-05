dc697848b52c6278c65eb05f589f1b0462a537cb
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 00:46:55 -0600

    Update 52n-QueensII.py

--- 52n-QueensII.py
+++ 52n-QueensII.py
@@ -1,18 +1,14 @@
 class Solution:
-    def count(self, column, left, right):
+    def count(self, column: int, left: int, right: int) -> int:
         count = 0
         available = self.nbit & ~(column | left | right)
         while available:
-            position = available & - available
+            position = available & -available
             available ^= position
-            count += self.count(column | position, (left | position) << 1, (right | position) >> 1)
-        count += column == self.nbit
+            count += self.count(position | column, (position | left) << 1, (position | right) >> 1)
+        count += self.nbit == column
         return count
-        
-    def totalNQueens(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    
+    def totalNQueens(self, n: int) -> int:
         self.nbit = (1 << n) - 1
         return self.count(0, 0, 0)
