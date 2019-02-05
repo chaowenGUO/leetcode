803f2cd263efe78e94c89ca5d73ddd850df84e83
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 04:14:53 -0600

    Update 260singleNumberIII.py

--- 260singleNumberIII.py
+++ 260singleNumberIII.py
@@ -1,14 +1,11 @@
+import typing
 class Solution:
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+    def singleNumber(self, nums: typing.List[int]) -> typing.List[int]:
         import functools, operator
         xor = functools.reduce(operator.xor, nums)
         position = xor & -xor
         result = [0] * 2
         for num in nums:
-            if num & position: result[0] ^= num
+            if position & num: result[0] ^= num
             else: result[1] ^= num
         return result
