78e4c986d52c1057824299b997e309fab2ef5017
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:01:01 -0600

    Update 791customSortString.py

--- 791customSortString.py
+++ 791customSortString.py
@@ -1,8 +1,3 @@
 class Solution:
-    def customSortString(self, S, T):
-        """
-        :type S: str
-        :type T: str
-        :rtype: str
-        """
+    def customSortString(self, S: str, T: str) -> str:
         return ''.join(sorted(T, key = lambda _: S.find(_)))
