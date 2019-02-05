e16804d497902c638272ff4e6d073f156aa67af9
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:03:57 -0600

    Update 832flippingAnImage.py

--- 832flippingAnImage.py
+++ 832flippingAnImage.py
@@ -1,7 +1,4 @@
+import typing
 class Solution:
-    def flipAndInvertImage(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        return [[_^ 1 for _ in reversed(row)] for row in A]
+    def flipAndInvertImage(self, A: typing.List[typing.List[int]]) -> typing.List[typing.List[int]]:
+        return [[_ ^ 1 for _ in reversed(row)] for row in A]
