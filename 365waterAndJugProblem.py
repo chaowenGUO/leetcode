1bb7ab672ca941b38dc5be985bb056282d01fec5
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 05:16:50 -0600

    Update 365waterAndJugProblem.py

--- 365waterAndJugProblem.py
+++ 365waterAndJugProblem.py
@@ -1,10 +1,4 @@
 class Solution:
-    def canMeasureWater(self, x, y, z):
-        """
-        :type x: int
-        :type y: int
-        :type z: int
-        :rtype: bool
-        """
+    def canMeasureWater(self, x: int, y: int, z: int) -> bool:
         import math
         return z == 0 or x + y >= z and z % math.gcd(x, y) == 0
