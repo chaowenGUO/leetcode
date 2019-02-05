6b85a38de7cf330a4b3c2ba8a74f5e3085ec7d4b
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 00:49:38 -0600

    Update 53maximumSubarray.py

--- 53maximumSubarray.py
+++ 53maximumSubarray.py
@@ -1,11 +1,8 @@
+import typing
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        result = dp = nums[0]
+    def maxSubArray(self, nums: typing.List[int]) -> int:
+        result = big = nums[0]
         for num in nums[1:]:
-            dp = max(dp + num, num)
-            result = max(result, dp)
+            big = max(num, num + big)
+            result = max(result, big)
         return result
