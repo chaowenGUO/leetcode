03cd719f4e8e56fe4d178337a56dd31460505dc0
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 02:38:57 -0600

    Update 100sameTree.py

--- 100sameTree.py
+++ 100sameTree.py
@@ -6,11 +6,6 @@
 #         self.right = None
 
 class Solution:
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p and q: return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else: return p == q
