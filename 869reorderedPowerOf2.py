ce8696468c935f5831ce1f62354b7bfbc5193dd4
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 07:18:22 -0600

    Update 869reorderedPowerOf2.py

--- 869reorderedPowerOf2.py
+++ 869reorderedPowerOf2.py
@@ -1,8 +1,4 @@
 class Solution:
-    def reorderedPowerOf2(self, N):
-        """
-        :type N: int
-        :rtype: bool
-        """
+    def reorderedPowerOf2(self, N: int) -> bool:
         import collections
         return any(collections.Counter(str(N)) == collections.Counter(str(1 << _)) for _ in range(31))
