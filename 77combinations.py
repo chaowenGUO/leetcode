283447e84b70685ee30bc5432aa9bbad1779ac33
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 02:27:36 -0600

    Update 77combinations.py

--- 77combinations.py
+++ 77combinations.py
@@ -1,9 +1,5 @@
+import typing
 class Solution:
-    def combine(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: List[List[int]]
-        """
+    def combine(self, n: int, k: int) -> typing.List[typing.List[int]]:
         import functools
-        return functools.reduce(lambda result, _:[[_] + combination for combination in result for _ in range(1, combination[0] if combination else n + 1)], range(k), [[]])
+        return functools.reduce(lambda result, _: [[_] + combination for combination in result for _ in range(1, combination[0] if combination else n + 1)], range(k), [[]])
