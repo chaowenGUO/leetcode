577d97fd40055c1ba862fcfde5010f889766a590
chaowen guo <chaowen.guo1@gmail.com>
2019-02-05 01:45:19 -0600

    Update 64minimumPathSum.py

--- 64minimumPathSum.py
+++ 64minimumPathSum.py
@@ -1,7 +1,7 @@
 import typing
 class Solution:
     def minPathSum(self, grid: typing.List[typing.List[int]]) -> int:
-        grid[0] = [*itertools.accumulate(grid[0])]
+        *grid[0], = itertools.accumulate(grid[0])
         for row in range(1, len(grid)): grid[row][0] += grid[row - 1][0]
         for row in range(1, len(grid)):
             for column in range(1, len(grid[0])): grid[row][column] += min(grid[row - 1][column], grid[row][column - 1])
