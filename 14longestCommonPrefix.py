d19de75be9ec7a193b1bbb1f72b01c1deaff90c6
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 03:47:18 -0600

    Update 14longestCommonPrefix.py

--- 14longestCommonPrefix.py
+++ 14longestCommonPrefix.py
@@ -1,11 +1,5 @@
+import typing
 class Solution:
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        result = ''
-        for char in zip(*strs):
-            if len({*char}) == 1: result += char[0]
-            else: break
-        return result
+    def longestCommonPrefix(self, strs: typing.List[str]) -> str:
+        import itertools
+        return ''.join(next(zip(*itertools.takewhile(lambda char: len({*char}) == 1, zip(*strs))), ''))
