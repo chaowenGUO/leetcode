5b1ddca651476c1310fa0672c187891a86847e09
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 05:01:57 -0600

    Update 60permutationSequence.py

--- 60permutationSequence.py
+++ 60permutationSequence.py
@@ -1,16 +1,11 @@
 class Solution:
-    def getPermutation(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: str
-        """
+    def getPermutation(self, n: int, k: int) -> str:
         import math
         factorial = math.factorial(n)
-        elements = [*range(1, n + 1)]
-        k, result = (k - 1) % factorial, ''
+        *elements, = range(1, n + 1)
+        result, k ='', (k - 1) % factorial
         while elements:
             factorial //= len(elements)
-            i, k = divmod(k, factorial)
-            result += str(elements.pop(i))
+            _, k = divmod(k, factorial)
+            result += str(elements.pop(_))
         return result
