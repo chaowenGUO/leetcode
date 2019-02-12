c3b4bcc0871f9b5b308f739db89555bebd03cf05
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 03:29:19 -0600

    Update 152maximumProductSubarray.py

--- 152maximumProductSubarray.py
+++ 152maximumProductSubarray.py
@@ -1,11 +1,8 @@
+import typing
 class Solution:
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def maxProduct(self, nums: typing.List[int]) -> int:
         result = big = small = nums[0]
         for num in nums[1:]:
-            big, small=max(num, num * big, num * small), min(num, num * big, num * small)
+            big, small = (_(num, num * big, num * small) for _ in (max, min))
             result = max(result, big)
         return result
