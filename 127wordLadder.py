e6b367c241063f3cd7c62b50a0aa9d703d298a6f
chaowen guo <chaowen.guo1@gmail.com>
2019-02-13 00:13:23 -0600

    Update 127wordLadder.py

--- 127wordLadder.py
+++ 127wordLadder.py
@@ -1,22 +1,17 @@
+import typing
 class Solution:
-    def ladderLength(self, beginWord, endWord, wordList):
-        """
-        :type beginWord: str
-        :type endWord: str
-        :type wordList: List[str]
-        :rtype: int
-        """
+    def ladderLength(self, beginWord: str, endWord: str, wordList: typing.List[str]) -> int:
         import collections
         wordList = {*wordList}
+        charSet = {char for word in wordList for char in word}
         queue = collections.deque([[beginWord, 1]])
-        wordSet = {char for word in wordList for char in word}
         while queue:
             word, length = queue.popleft()
             if word == endWord: return length
             else:
                 for _ in range(len(word)):
-                    for char in wordSet:
-                        nextWord = word[:_] + char + word[_ + 1:]
+                    for char in charSet:
+                        nextWord = ''.join((word[:_], char, word[_ + 1:]))
                         if nextWord in wordList:
                             wordList.remove(nextWord)
                             queue += [nextWord, length + 1],
