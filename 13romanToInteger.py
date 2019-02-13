15c6ac12166e5a10aa20fe4ab61173cd15ad69b8
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 21:17:55 -0600

    Update 13romanToInteger.py

--- 13romanToInteger.py
+++ 13romanToInteger.py
@@ -1,10 +1,5 @@
 class Solution:
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def romanToInt(self, s: str) -> int:
+        import functools
         dictionary = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
-        result, p = 0, 'I'
-        for char in reversed(s): result, p = result - dictionary.get(char) if dictionary.get(char) < dictionary.get(p) else result + dictionary.get(char), char
-        return result
+        return functools.reduce(lambda result, char: [result[0] - dictionary.get(char) if dictionary.get(char) < dictionary.get(result[1]) else result[0] + dictionary.get(char), char], reversed(s), [0, 'I'])[0]
