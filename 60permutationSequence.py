bc860f6aaf2eec18bb80bbee5bcc9794f2aae0d5
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 23:43:07 -0600

    Update 60permutationSequence.py

--- 60permutationSequence.py
+++ 60permutationSequence.py
@@ -2,8 +2,8 @@ class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         import math
         factorial = math.factorial(n)
-        *elements, = range(1, n + 1)
         result, k = '', (k - 1) % factorial
+        *elements, = range(1, n + 1)
         while elements:
             factorial //= len(elements)
             _, k = divmod(k, factorial)
