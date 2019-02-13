b82cdf75029e4296ede94acb0df23c4849ed0b25
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 22:03:46 -0600

    Update 914xOfAKindInADeckOfCards.py

--- 914xOfAKindInADeckOfCards.py
+++ 914xOfAKindInADeckOfCards.py
@@ -1,8 +1,5 @@
+import typing
 class Solution:
-    def hasGroupsSizeX(self, deck):
-        """
-        :type deck: List[int]
-        :rtype: bool
-        """
+    def hasGroupsSizeX(self, deck: typing.List[int]) -> bool:
         import functools, math, collections
         return functools.reduce(math.gcd, collections.Counter(deck).values()) > 1
