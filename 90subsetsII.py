f1022ec7e2da0b1331e38dbaea8be959a42ef0ad
chaowen guo <chaowen.guo1@gmail.com>
2019-02-12 21:35:20 -0600

    Update 90subsetsII.py

--- 90subsetsII.py
+++ 90subsetsII.py
@@ -1,8 +1,5 @@
+import typing
 class Solution:
-    def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+    def subsetsWithDup(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
         import functools
         return functools.reduce(lambda result, num: result + [_ + [num] for _ in result if _ + [num] not in result], sorted(nums), [[]])
