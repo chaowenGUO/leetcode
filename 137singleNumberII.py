f15b7d78a86ed709df7f5db7a89b2d0f1a1f2e64
chaowen guo <chaowen.guo1@gmail.com>
2019-02-14 19:23:01 -0600

    Update 137singleNumberII.py

--- 137singleNumberII.py
+++ 137singleNumberII.py
@@ -1,11 +1,8 @@
+import typing
 class Solution:
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def singleNumber(self, nums: typing.List[int) -> int:
         a = b = 0
         for num in nums:
-            a = (a ^ num) & b
-            b = (b ^ num) | a
+            a = (a ^ num) | b
+            b = (b ^ num) & a
         return b
