baab5ee775c0b2289a752d9440ab193fb48207f3
chaowen guo <chaowen.guo1@gmail.com>
2019-02-16 09:05:56 -0600

    Update 559maximumDepthOfN-aryTree.py

--- 559maximumDepthOfN-aryTree.py
+++ 559maximumDepthOfN-aryTree.py
@@ -1,15 +1,10 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val, children):
         self.val = val
         self.children = children
 """
 class Solution:
-    def maxDepth(self, root):
-        """
-        :type root: Node
-        :rtype: int
-        """
-        import itertools
-        return max(itertools.chain(map(self.maxDepth, root.children), [0])) + 1 if root else 0
+    def maxDepth(self, root: Node) -> int:
+        return max(map(self.maxDepth, root.children), default = 0) + 1 if root else 0
