acdf7001d7524d72ed487de4c15fe72a9430f086
chaowen guo <chaowen.guo1@gmail.com>
2019-02-16 09:31:24 -0600

    Update 605canPlaceFlowers.py

--- 605canPlaceFlowers.py
+++ 605canPlaceFlowers.py
@@ -1,12 +1,8 @@
+import typing
 class Solution:
-    def canPlaceFlowers(self, flowerbed, n):
-        """
-        :type flowerbed: List[int]
-        :type n: int
-        :rtype: bool
-        """
+    def canPlaceFlowers(self, flowerbed: typing.List[int], n: int) -> bool:
+        flowerbed = [0, *flowerbed, 0]
         count = 0
-        flowerbed = [0] + flowerbed + [0]
         for _ in range(1, len(flowerbed) - 1):
             if flowerbed[_ - 1] == flowerbed[_] == flowerbed[_ + 1] == 0:
                 flowerbed[_] = 1
