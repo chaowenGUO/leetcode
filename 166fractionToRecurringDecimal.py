b6de629d2274e3cf828ef16a59f50265051424d5
chaowen guo <chaowen.guo1@gmail.com>
2019-02-18 05:08:15 -0600

    Update 166fractionToRecurringDecimal.py

--- 166fractionToRecurringDecimal.py
+++ 166fractionToRecurringDecimal.py
@@ -1,18 +1,13 @@
 class Solution:
-    def fractionToDecimal(self, numerator, denominator):
-        """
-        :type numerator: int
-        :type denominator: int
-        :rtype: str
-        """
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         sign = '-' if numerator * denominator < 0 else ''
         head, remainder = divmod(abs(numerator), abs(denominator))
         tail, seen = '', {}
         while remainder:
             if remainder in seen:
-                tail = tail[:seen[remainder]] + '(' + tail[seen[remainder]:] + ')'
+                tail = ''.join((tail[:seen[remainder]], '(', tail[seen[remainder]:], ')'))
                 break
             seen[remainder] = len(tail)
             digit, remainder = divmod(remainder * 10, abs(denominator))
             tail += str(digit)
-        return sign + str(head) + (tail and '.' + tail)
+        return ''.join((sign, str(head), (tail and '.' + tail)))
