7fe843aa52547421e07185b050fce044cd3e7fb8
chaowen guo <chaowen.guo1@gmail.com>
2019-02-24 19:33:04 -0600

    Update 404sumOfLeftLeaves.py

--- 404sumOfLeftLeaves.py
+++ 404sumOfLeftLeaves.py
@@ -9,4 +9,4 @@ class Solution:
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
         if not root: return 0
         elif root.left and not root.left.left and not root.left.right: return root.left.val + self.sumOfLeftLeaves(root.right)
-        else: return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        else: return sum(map(self.sumOfLeftLeaves, (root.left, root.right)))
