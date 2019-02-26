3437924229f33c3b09376a5d6a7b477131723a43
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 20:03:04 -0600

    Update 931minimumFallingPathSum.py

--- 931minimumFallingPathSum.py
+++ 931minimumFallingPathSum.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def minFallingPathSum(self, A: typing.List[typing.List[int]]) -> int:
-        import functools
+    def minFallingPathSum(self, A: List[List[int]]) -> int:
         return min(functools.reduce(lambda dp, row: [value + min(dp[max(0, column - 1)], dp[column], dp[min(column + 1, len(A) - 1)]) for column, value in enumerate(row)], A))
