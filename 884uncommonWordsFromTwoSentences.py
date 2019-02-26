d53616b9dfc71bd5860af3ba67e1a143cf67bc3c
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 19:44:30 -0600

    Update 884uncommonWordsFromTwoSentences.py

--- 884uncommonWordsFromTwoSentences.py
+++ 884uncommonWordsFromTwoSentences.py
@@ -1,5 +1,3 @@
-import typing
 class Solution:
-    def uncommonFromSentences(self, A: str, B: str) -> typing.List[str]:
-        import collections
-        return [_[0] for _ in collections.Counter((A + ' ' + B).split()).items() if _[1] == 1]
+    def uncommonFromSentences(self, A: str, B: str) -> List[str]:
+        return [_[0] for _ in collections.Counter(''.join((A, ' ', B)).split()).items() if _[1] == 1]
