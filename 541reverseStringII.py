665abdc738f7bbf2ae708d6308217f1827eff038
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 21:04:40 -0600

    Update 541reverseStringII.py

--- 541reverseStringII.py
+++ 541reverseStringII.py
@@ -1,10 +1,5 @@
 class Solution:
-    def reverseStr(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
-        s = [*s]
+    def reverseStr(self, s: str, k: int) -> str:
+        *s, = s
         for _ in range(0, len(s), 2 * k): s[_:_ + k] = reversed(s[_:_ + k])
         return ''.join(s)
