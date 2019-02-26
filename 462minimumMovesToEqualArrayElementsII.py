36f92d3aa080a0807c672e229e0d382b7e0a50b0
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 21:15:08 -0600

    Update 462minimumMovesToEqualArrayElementsII.py

--- 462minimumMovesToEqualArrayElementsII.py
+++ 462minimumMovesToEqualArrayElementsII.py
@@ -1,5 +1,4 @@
-import typing
 class Solution:
-    def minMoves2(self, nums: typing.List[int]) -> int:
+    def minMoves2(self, nums: List[int]) -> int:
         nums = sorted(nums)
         return sum(nums[~_] - nums[_] for _ in range(len(nums) // 2))
