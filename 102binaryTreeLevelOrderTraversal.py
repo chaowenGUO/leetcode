7a9b5cb40df1ea3bf97526cd9af6f2e357c3e370
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 18:06:38 -0600

    Update 102binaryTreeLevelOrderTraversal.py

--- 102binaryTreeLevelOrderTraversal.py
+++ 102binaryTreeLevelOrderTraversal.py
@@ -5,11 +5,11 @@
 #         self.left = None
 #         self.right = None
 
-import typing
 class Solution:
-    def levelOrder(self, root: TreeNode) -> typing.List[typing.List[int]]:
-        result, level = [], [root]
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        result, level, direction = [], [root], 1
         while root and level:
-            result += [node.val for node in level],
+            result += [node.val for node in level][::direction],
+            direction *= -1
             level = [child for node in level for child in (node.left, node.right) if child]
         return result
