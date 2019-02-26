2676b14ee8d7c373e7ddabe0d63854be0bc53292
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 21:03:50 -0600

    Update 543diameterOfBinaryTree.py

--- 543diameterOfBinaryTree.py
+++ 543diameterOfBinaryTree.py
@@ -7,14 +7,10 @@
 
 class Solution:
     def diameterDepth(self, root):
-        if not root: return 0, 0
+        if not root: return (0,) * 2
         else:
             left, right = map(self.diameterDepth, (root.left, root.right))
             return max(left[0], right[0], left[1] + right[1]), max(left[1], right[1]) + 1
-    
-    def diameterOfBinaryTree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+        
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
         return self.diameterDepth(root)[0]
