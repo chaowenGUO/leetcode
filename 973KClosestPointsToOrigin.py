d073973c35f341c100636c9c30bf439cb47b6b4d
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 19:56:42 -0600

    Update 973KClosestPointsToOrigin.py

--- 973KClosestPointsToOrigin.py
+++ 973KClosestPointsToOrigin.py
@@ -1,5 +1,3 @@
-import typing
 class Solution:
-    def kClosest(self, points: typing.List[typing.List[int]], K: int) -> typing.List[typing.List[int]]:
-        import heapq
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         return heapq.nsmallest(K, points, lambda distance: distance[0]**2 + distance[1]**2)
