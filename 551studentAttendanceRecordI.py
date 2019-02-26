ef3b3d05b656a012815841f44136fa2d37ea8a7e
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 20:43:58 -0600

    Update 551studentAttendanceRecordI.py

--- 551studentAttendanceRecordI.py
+++ 551studentAttendanceRecordI.py
@@ -1,7 +1,3 @@
 class Solution:
-    def checkRecord(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+    def checkRecord(self, s: str) -> bool:
         return s.count('A') <= 1 and 'L' * 3 not in s
