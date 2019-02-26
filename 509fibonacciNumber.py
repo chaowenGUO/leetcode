e6b7a00e8dbe1e3f2c6c5dcdaa55875268ec8734
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 18:30:28 -0600

    Update 509fibonacciNumber.py

--- 509fibonacciNumber.py
+++ 509fibonacciNumber.py
@@ -1,4 +1,5 @@
+import functools
+
 class Solution:
     def fib(self, N: int) -> int:
-        import functools
         return functools.reduce(lambda result, _: [result[1], sum(result)], range(N), [0, 1])[0]
