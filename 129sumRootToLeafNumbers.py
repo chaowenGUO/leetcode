637ee3d0a6be545f50b2ad74a16993d3623fc686
chaowen guo <chaowen.guo1@gmail.com>
2019-02-25 20:36:58 -0600

    Update 129sumRootToLeafNumbers.py

--- 129sumRootToLeafNumbers.py
+++ 129sumRootToLeafNumbers.py
@@ -6,10 +6,10 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, root: TreeNode, s: int) -> int:
+    def dfs(self, root, s):
         if not root: return 0
         elif not root.left and not root.right: return root.val + s * 10
-        else: return self.dfs(root.left, root.val + s * 10) + self.dfs(root.right, root.val + s * 10)
+        else: return sum(map(self.dfs, (root.left, root.right), (root.val + s * 10,) * 2))
         
     def sumNumbers(self, root: TreeNode) -> int:
         return self.dfs(root, 0)
