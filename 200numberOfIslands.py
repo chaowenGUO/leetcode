29c330995f1f16102fe5e32862432c6692ab561d
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 17:32:05 -0600

    Update 200numberOfIslands.py

--- 200numberOfIslands.py
+++ 200numberOfIslands.py
@@ -1,18 +1,10 @@
-import typing
 class Solution:
-    def sink(self, grid: typing.List[typing.List[str]], row: int, column: int) -> None:
-        if 0 <= row < len(grid) and 0 <= column < len(grid[0]) and grid[row][column] == '1':
-            grid[row][column] = '0'
-            self.sink(grid, row + 1, column)
-            self.sink(grid, row - 1, column)
-            self.sink(grid, row, column + 1)
-            self.sink(grid, row, column - 1)
+    def sink(self, grid, row, column):
+            if 0 <= row < len(grid) and 0 <= column < len(grid[row]) and grid[row][column] == '1':
+                grid[row][column] = '0'
+                for _ in map(self.sink, (grid,) * 4, (row + 1, row - 1, row, row), (column, column, column + 1, column - 1)): pass
+                return 1
+            return 0
         
-    def numIslands(self, grid: typing.List[typing.List[str]]) -> int:
-        result = 0
-        for row in range(len(grid)):
-            for column in range(len(grid[0])):
-                if grid[row][column] == '1':
-                    self.sink(grid, row, column)
-                    result += 1
-        return result
+    def numIslands(self, grid: List[List[str]]) -> int:     
+        return sum(self.sink(grid, row, column) for row in range(len(grid)) for column in range(len(grid[row])))
