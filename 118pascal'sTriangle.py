eb33a6de96632233743b2a5c81ae54dd89579c88
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 14:15:44 -0600

    Update 118pascal'sTriangle.py

--- 118pascal'sTriangle.py
+++ 118pascal'sTriangle.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def generate(self, numRows: int) -> typing.List[typing.List[int]]:
-        import functools
-        return functools.reduce(lambda result, _: result + [[sum(_) for _ in zip([0] + result[-1], result[-1] + [0])]], range(numRows - 1), [[1]])[:numRows]
+    def generate(self, numRows: int) -> List[List[int]]:
+        return functools.reduce(lambda result, _: result + [[sum(_) for _ in zip(itertools.chain([0], result[-1]), itertools.chain(result[-1], [0]))]], range(numRows - 1), [[1]])[:numRows]
