76465afda2b3f78da6d7769cf2eaac40ece91484
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 14:18:43 -0600

    Update 463islandPerimeter.py

--- 463islandPerimeter.py
+++ 463islandPerimeter.py
@@ -1,10 +1,3 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        result = 0
-        for row in range(len(grid)):
-            for column in range(len(grid[0])):
-                if grid[row][column] == 1:
-                    result += 4
-                    if row > 0 and grid[row - 1][column] == 1: result -= 2
-                    if column > 0 and grid[row][column - 1] == 1: result -= 2
-        return result
+        return sum(sum(map(operator.ne, itertools.chain([0], row), itertools.chain(row, [0]))) for row in itertools.chain(grid, map(list, zip(*grid))))
