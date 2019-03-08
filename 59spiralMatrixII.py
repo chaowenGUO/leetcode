d67a5bff6b52f98e1007f7a2c98c72bc87c886bf
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 19:24:59 -0600

    Update 59spiralMatrixII.py

--- 59spiralMatrixII.py
+++ 59spiralMatrixII.py
@@ -1,6 +1,5 @@
-import typing
 class Solution:
-    def generateMatrix(self, n: int) -> typing.List[typing.List[int]]:
+    def generateMatrix(self, n: int) -> List[List[int]]:
         result = [[n**2]]
         while result[0][0] > 1: result = [[*range(result[0][0] - len(result), result[0][0])], *map(list, zip(*reversed(result)))]
         return result
