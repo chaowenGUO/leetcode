6bb7d2ee473e127bb07e123dc034eb57a1613902
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 19:01:56 -0600

    Update 47permutationsII.py

--- 47permutationsII.py
+++ 47permutationsII.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def permuteUnique(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
-        import functools
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         return functools.reduce(lambda result, num: [[*permutation[:_], num, *permutation[_:]] for permutation in result for _ in range([*permutation, num].index(num) + 1)], nums, [[]])
