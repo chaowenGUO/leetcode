45640fc0a940f3551cd2849e5f6ad4f14240bfda
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 18:59:14 -0600

    Update 46permutations.py

--- 46permutations.py
+++ 46permutations.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def permute(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
-        import functools
+    def permute(self, nums: List[int]) -> List[List[int]]:
         return functools.reduce(lambda result, num: [[*permutation[:_], num, *permutation[_:]] for permutation in result for _ in range(len(permutation) + 1)], nums, [[]])
