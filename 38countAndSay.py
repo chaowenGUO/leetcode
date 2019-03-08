3691bae5e7bae2b2cfd373b1aebccc8e4b1449d9
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 18:51:21 -0600

    Update 38countAndSay.py

--- 38countAndSay.py
+++ 38countAndSay.py
@@ -1,4 +1,5 @@
+import functools
+
 class Solution:
     def countAndSay(self, n: int) -> str:
-        import functools, itertools
         return functools.reduce(lambda result, _: ''.join(str(sum(1 for _ in group)) + key for key, group in itertools.groupby(result)), range(n - 1), '1')
