d7fa05df1afe972a59d3ec7ded49af4fae3ee751
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 19:14:39 -0600

    Update 55jumpGame.py

--- 55jumpGame.py
+++ 55jumpGame.py
@@ -1,6 +1,5 @@
-import typing
 class Solution:
-    def canJump(self, nums: typing.List[int]) -> bool:
+    def canJump(self, nums: List[int]) -> bool:
         reach = 0
         for index, num in enumerate(nums):
             if index <= reach: reach = max(reach, index + num)
