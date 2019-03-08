6083ae87debebd9ce79a7232c811761d420f9fa0
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 18:04:37 -0600

    Update 11containerWithMostWater.py

--- 11containerWithMostWater.py
+++ 11containerWithMostWater.py
@@ -1,10 +1,9 @@
-import typing
 class Solution:
-    def maxArea(self, height: typing.List[int]) -> int:
+    def maxArea(self, height: List[int]) -> int:
         left, right = 0, len(height) - 1
         result = 0
         while left < right:
             result = max(result, (right - left) * min(height[left], height[right]))
             if height[left] < height[right]: left += 1
             else: right -= 1
-        return result 
+        return result
