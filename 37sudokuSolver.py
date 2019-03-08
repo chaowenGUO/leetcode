d9d7dbea83ac9793dbf34bfc8e656b9c4d85fa3c
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 18:51:53 -0600

    Update 37sudokuSolver.py

--- 37sudokuSolver.py
+++ 37sudokuSolver.py
@@ -1,21 +1,19 @@
-import typing
 class Solution:
-    import string
-    def dfs(self, board: typing.List[typing.List[str]], stack: typing.List[typing.Tuple[int, int]]) -> None:
+    def dfs(self, board, stack):
         if not stack: return
         x, y = stack.pop()
         box = [board[x // 3 * 3 + i][y // 3 * 3 + j] for i in range(3) for j in range(3)]
         row = [board[_][y] for _ in range(9)]
         column = [board[x][_] for _ in range(9)]
         for _ in string.digits[1:]:
-            if not any([_ in box, _ in row, _ in column]):
+            if not any((_ in box, _ in row, _ in column)):
                 board[x][y] = _
                 self.dfs(board, stack)
                 if not stack: return
         board[x][y] = '.'
         stack += (x, y),
         
-    def solveSudoku(self, board: typing.List[typing.List[str]]) -> None:
+    def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
