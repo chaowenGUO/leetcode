0ea3b49d84f483006c50b69805014dde0707302b
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 18:44:22 -0600

    Update 36validSudoku.py

--- 36validSudoku.py
+++ 36validSudoku.py
@@ -1,6 +1,5 @@
-import typing
 class Solution:
-    def isValidSudoku(self, board: typing.List[typing.List[str]]) -> bool:
-        import itertools
-        *seen, = itertools.chain(*([(column, j), (i, column), (i // 3, j // 3, column)] for i, row in enumerate(board) for j, column in enumerate(row) if column != '.'))
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = ([(column, j), (i, column), (i // 3, j // 3, column)] for i, row in enumerate(board) for j, column in enumerate(row) if column != '.')
+        *seen, = itertools.chain(*seen)
         return len(seen) == len({*seen})
