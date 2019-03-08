f5e23e5d9a31c9881d0db039b9cef7a120ff36d4
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 19:22:45 -0600

    Update 58lengthOfLastWord.py

--- 58lengthOfLastWord.py
+++ 58lengthOfLastWord.py
@@ -1,4 +1,5 @@
+import functools
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        import functools, itertools, operator
         return sum(1 for _ in itertools.takewhile(functools.partial(operator.ne, ' '), reversed(s.strip())))
