498314e8b72195447a1f65b3532a3a845cddd179
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 21:35:27 -0600

    Update 113pathSumII.py

--- 113pathSumII.py
+++ 113pathSumII.py
@@ -9,4 +9,4 @@ class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
         if not root: return []
         elif not root.left and not root.right and sum == root.val: return [[root.val]]
-        else: return [[root.val] + _ for _ in self.pathSum(root.left, sum - root.val) + self.pathSum(root.right, sum - root.val)]
+        else: return [[root.val] + _ for _ in itertools.chain(*map(self.pathSum, (root.left, root.right), (sum - root.val,) * 2))]
