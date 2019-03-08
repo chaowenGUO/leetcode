64c029e7c73d69021089b23fb29ef690acfcd989
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 21:51:08 -0600

    Update 124binaryTreeMaximumPathSum.py

--- 124binaryTreeMaximumPathSum.py
+++ 124binaryTreeMaximumPathSum.py
@@ -6,19 +6,15 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, root): # returns: max one side path sum, max path sum
+    def dfs(self, root):
         left = right = [0, root.val]
         if root.left:
             left = self.dfs(root.left)
-            left[0] = max(left[0], 0)
+            left[0] = max(0, left[0])
         if root.right:
             right = self.dfs(root.right)
-            right[0] = max(right[0], 0)
+            right[0] = max(0, right[0])
         return [root.val + max(left[0], right[0]), max(root.val + left[0] + right[0], left[1], right[1])]
     
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+    def maxPathSum(self, root: TreeNode) -> int:
         return self.dfs(root)[1]
