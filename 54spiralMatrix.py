7609372e6918f779e793da2ab2cb5fa77179e16c
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 19:12:54 -0600

    Update 54spiralMatrix.py

--- 54spiralMatrix.py
+++ 54spiralMatrix.py
@@ -1,7 +1,3 @@
 class Solution:
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        return matrix and matrix.pop(0) + self.spiralOrder([*map(list,zip(*matrix))][::-1])
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        return matrix and matrix.pop(0) + self.spiralOrder([*map(list, zip(*matrix))][::-1])
