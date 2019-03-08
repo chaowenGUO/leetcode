9bcf1d0a605107d70db5e63abe81bf9270a38e8e
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 21:01:18 -0600

    Update 113pathSumII.py

--- 113pathSumII.py
+++ 113pathSumII.py
@@ -5,9 +5,8 @@
 #         self.left = None
 #         self.right = None
 
-import typing
 class Solution:
-    def pathSum(self, root: TreeNode, sum: int) -> typing.List[typing.List[int]]:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
         if not root: return []
         elif not root.left and not root.right and sum == root.val: return [[root.val]]
         else: return [[root.val] + _ for _ in self.pathSum(root.left, sum - root.val) + self.pathSum(root.right, sum - root.val)]
