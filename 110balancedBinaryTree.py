8dbbb050802a7d2c6d0c656e87797913fed3402b
chaowen guo <chaowen.guo1@gmail.com>
2019-03-07 20:56:29 -0600

    Update 110balancedBinaryTree.py

--- 110balancedBinaryTree.py
+++ 110balancedBinaryTree.py
@@ -6,11 +6,11 @@
 #         self.right = None
 
 class Solution:
-    def check(self, root: TreeNode) -> int:
+    def check(self, root):
         if not root: return 0
         else:
             left, right = map(self.check, (root.left, root.right))
             return -1 if left == -1 or right == -1 or abs(left - right) > 1 else 1 + max(left, right)
-            
+        
     def isBalanced(self, root: TreeNode) -> bool:
         return self.check(root) != -1
