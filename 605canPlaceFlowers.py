f71b4fd80c07274927f3b20f99f27c1a0836e2e2
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 23:15:39 -0600

    Update 605canPlaceFlowers.py

--- 605canPlaceFlowers.py
+++ 605canPlaceFlowers.py
@@ -1,6 +1,5 @@
-import typing
 class Solution:
-    def canPlaceFlowers(self, flowerbed: typing.List[int], n: int) -> bool:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         flowerbed = [0, *flowerbed, 0]
         count = 0
         for _ in range(1, len(flowerbed) - 1):
