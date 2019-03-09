e2e23e4c3639d02eed98c23168a52e0e4734a178
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 21:54:14 -0600

    Update 932beautifulArray.py

--- 932beautifulArray.py
+++ 932beautifulArray.py
@@ -1,7 +1,3 @@
 class Solution:
-    def beautifulArray(self, N):
-        """
-        :type N: int
-        :rtype: List[int]
-        """
-        return sorted(range(1, N + 1), key = lambda _:bin(_)[:1:-1])
+    def beautifulArray(self, N: int) -> List[int]:
+        return sorted(range(1, N + 1), key = lambda _: bin(_)[:1:-1])
