75388c2c8182a0192f2190f2dc29ece03276c623
chaowen guo <chaowen.guo1@gmail.com>
2019-03-09 00:15:43 -0600

    Update 397integerReplacement.py

--- 397integerReplacement.py
+++ 397integerReplacement.py
@@ -1,9 +1,5 @@
 class Solution:
-    def integerReplacement(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def integerReplacement(self, n: int) -> int:
         result = 0
         while n > 1:
             if n & 1 == 0: n >>= 1
