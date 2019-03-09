539629b6e5261e5f5ae16a811c94899971c4e46a
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 23:44:37 -0600

    Update 434numberOfSegmentsInAString.py

--- 434numberOfSegmentsInAString.py
+++ 434numberOfSegmentsInAString.py
@@ -1,7 +1,3 @@
 class Solution:
-    def countSegments(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def countSegments(self, s: str) -> int:
         return sum(s[_] != ' ' and (_ == 0 or s[_ - 1] == ' ') for _ in range(len(s)))
