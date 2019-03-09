6e8a936d577442ed77c81309cd976478f8877672
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 22:39:18 -0600

    Update 700searchInABinarySearchTree.py

--- 700searchInABinarySearchTree.py
+++ 700searchInABinarySearchTree.py
@@ -6,14 +6,10 @@
 #         self.right = None
 
 class Solution:
-    def searchBST(self, root, val):
-        """
-        :type root: TreeNode
-        :type val: int
-        :rtype: TreeNode
-        """
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
         node = root
         while node:
             if node.val == val: return node
             elif node.val < val: node = node.right
             else: node = node.left
+        return None
