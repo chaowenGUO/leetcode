623bb46da3138efa439aa5c7279d3ada0ba304ee
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 23:49:56 -0600

    Update 451sortCharactersByFrequency.py

--- 451sortCharactersByFrequency.py
+++ 451sortCharactersByFrequency.py
@@ -1,4 +1,3 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        import collections, operator
         return ''.join(operator.mul(*_) for _ in collections.Counter(s).most_common())
