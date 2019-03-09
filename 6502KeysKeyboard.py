5d73129982c1eeaf683510eaaba0bcf9e064b6a4
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 22:48:00 -0600

    Update 6502KeysKeyboard.py

--- 6502KeysKeyboard.py
+++ 6502KeysKeyboard.py
@@ -1,12 +1,8 @@
 class Solution:
-    def minSteps(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def minSteps(self, n: int) -> int:
         result = 0
         for _ in range(2, n + 1):
-            while n % _ == 0:
+            while not n % _:
                 result += _
                 n //= _
         return result
