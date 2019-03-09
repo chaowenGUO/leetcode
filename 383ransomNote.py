84ea0cc46db7e8faf250d72ea4484625144cbb39
chaowen guo <chaowen.guo1@gmail.com>
2019-03-09 00:03:37 -0600

    Update 383ransomNote.py

--- 383ransomNote.py
+++ 383ransomNote.py
@@ -1,4 +1,3 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        import collections
         return not collections.Counter(ransomNote) - collections.Counter(magazine)
