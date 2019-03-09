d18f6b6713e6a8ee54ee3e3c5171dbc5ab5a8d55
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 23:04:59 -0600

    Update 572subtreeOfAnotherTree.py

--- 572subtreeOfAnotherTree.py
+++ 572subtreeOfAnotherTree.py
@@ -1,14 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def isSame(self, p, q):
         if p and q: return p.val == q.val and self.isSame(p.left, q.left) and self.isSame(p.right, q.right)
         else: return p == q
         
-    def isSubtree(self, s, t):
-        """
-        :type s: TreeNode
-        :type t: TreeNode
-        :rtype: bool
-        """
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
         if not s: return False
         elif self.isSame(s, t): return True
         else: return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
