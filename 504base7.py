54f09f6461416f2341863206c86ac1b2d7c66ad1
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 23:12:36 -0600

    Update 504base7.py

--- 504base7.py
+++ 504base7.py
@@ -1,14 +1,11 @@
 class Solution:
-    def convertToBase7(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
+    def convertToBase7(self, num: int) -> str:
         if not num: return '0'
-        result = ''
-        sign = '' if num > 0 else '-'
-        num = abs(num)
-        while num:
-            result = str(num % 7) + result
-            num //= 7
-        return sign + result
+        else:
+            sign = '' if num > 0 else '-'
+            num = abs(num)
+            result = ''
+            while num:
+                result = str(num % 7) + result
+                num //= 7
+            return sign + result
