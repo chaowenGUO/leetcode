7606ce04412b10f6f63c1d24890c06abb45da7c6
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 22:57:32 -0600

    Update 594longestHarmoniousSubsequence.py

--- 594longestHarmoniousSubsequence.py
+++ 594longestHarmoniousSubsequence.py
@@ -1,9 +1,4 @@
 class Solution:
-    def findLHS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        import collections
+    def findLHS(self, nums: List[int]) -> int:
         dictionary = collections.Counter(nums)
         return max((dictionary.get(_) + dictionary.get(_ + 1) for _ in dictionary if _ + 1 in dictionary), default = 0)
