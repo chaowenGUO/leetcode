fd9bf698a8ac0a2558a0be0163a2c5f76ca4e5c7
chaowen guo <chaowen.guo1@gmail.com>
2019-03-08 21:46:26 -0600

    Update 908smallestRangeI.py

--- 908smallestRangeI.py
+++ 908smallestRangeI.py
@@ -1,4 +1,3 @@
-import typing
 class Solution:
-    def smallestRangeI(self, A: typing.List[int], K: int) -> int:
+    def smallestRangeI(self, A: List[int], K: int) -> int:
         return max(0, max(A) - min(A) - 2 * K)
