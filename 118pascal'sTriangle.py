6ab0310c00c9f547f114c78f4289ebee7d7a0b35
chaowen guo <chaowen.guo1@gmail.com>
2019-03-09 23:58:11 -0600

    Update 118pascal'sTriangle.py

--- 118pascal'sTriangle.py
+++ 118pascal'sTriangle.py
@@ -2,4 +2,4 @@ import functools
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        return functools.reduce(lambda result, _: result + [[sum(_) for _ in zip(itertools.chain([0], result[-1]), itertools.chain(result[-1], [0]))]], range(numRows - 1), [[1]])[:numRows]
+        return functools.reduce(lambda result, _: [*result, [*map(operator.add, itertools.chain([0], result[-1]), itertools.chain(result[-1], [0]))]], range(numRows - 1), [[1]])[:numRows]
