628cdbecf6e2ae0ab206459f319f92a05ea2594d
chaowen guo <chaowen.guo1@gmail.com>
2019-03-10 21:38:02 -0500

    Update 90subsetsII.py

--- 90subsetsII.py
+++ 90subsetsII.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def subsetsWithDup(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
-        import functools
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         return functools.reduce(lambda result, num: result + [_ + [num] for _ in result if _ + [num] not in result], sorted(nums), [[]])
