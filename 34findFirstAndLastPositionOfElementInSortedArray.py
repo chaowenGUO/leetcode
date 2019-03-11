543f9c149c09d4bb99e5109c41e79bd65289eed1
chaowen guo <chaowen.guo1@gmail.com>
2019-03-10 20:51:06 -0500

    Update 34findFirstAndLastPositionOfElementInSortedArray.py

--- 34findFirstAndLastPositionOfElementInSortedArray.py
+++ 34findFirstAndLastPositionOfElementInSortedArray.py
@@ -1,6 +1,5 @@
-import typing
 class Solution:
-    def lower(self, nums: typing.List[int], target: int) -> typing.List[int]:
+    def lower(self, nums, target):
         left, right = 0, len(nums) - 1
         while left + 1 < right:
             middle = left + (right - left) // 2
@@ -10,7 +9,7 @@ class Solution:
         elif nums[right] == target: return right
         else: return -1
         
-    def upper(self, nums: typing.List[int], target: int) -> typing.List[int]:
+    def upper(self, nums, target):
         left, right = 0, len(nums) - 1
         while left + 1 < right:
             middle = left + (right - left) // 2
@@ -20,5 +19,5 @@ class Solution:
         elif nums[left] == target: return left
         else: return -1
         
-    def searchRange(self, nums: typing.List[int], target: int) -> typing.List[int]:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
         return [_(nums, target) for _ in (self.lower, self.upper)] if nums else [-1] * 2
