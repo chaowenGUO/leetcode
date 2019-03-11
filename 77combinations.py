6320adade4cd765100c84de618703dbeac8cd5d3
chaowen guo <chaowen.guo1@gmail.com>
2019-03-10 21:24:37 -0500

    Update 77combinations.py

--- 77combinations.py
+++ 77combinations.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def combine(self, n: int, k: int) -> typing.List[typing.List[int]]:
-        import functools
+    def combine(self, n: int, k: int) -> List[List[int]]:
         return functools.reduce(lambda result, _: [[_] + combination for combination in result for _ in range(1, combination[0] if combination else n + 1)], range(k), [[]])
