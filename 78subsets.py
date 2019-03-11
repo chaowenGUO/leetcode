9d060be618ab2d42f062425dcff9aee3d095f517
chaowen guo <chaowen.guo1@gmail.com>
2019-03-10 21:26:16 -0500

    Update 78subsets.py

--- 78subsets.py
+++ 78subsets.py
@@ -1,5 +1,5 @@
-import typing
+import functools
+
 class Solution:
-    def subsets(self, nums: typing.List[int]) -> typing.List[typing.List[int]]:
-        import functools
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         return functools.reduce(lambda result, num: result + [_ + [num] for _ in result], nums, [[]])
