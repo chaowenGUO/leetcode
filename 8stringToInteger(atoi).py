34dcf8559d2e5be8d73b1dbdf0185e6e5ac58fbd
chaowen guo <chaowen.guo1@gmail.com>
2019-03-17 19:20:02 -0500

    Update 8stringToInteger(atoi).py

--- 8stringToInteger(atoi).py
+++ 8stringToInteger(atoi).py
@@ -2,5 +2,5 @@ class Solution:
     def myAtoi(self, str: str) -> int:
         try:
             result = int(re.search('^[+-]?\d+', str.strip()).group())
-            return max(-(1 << 31), min((1 << 31) - 1, result))
+            return max(-1 << 31, min((1 << 31) - 1, result))
         except: return 0
