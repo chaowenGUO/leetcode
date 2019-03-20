217ad50bc3ea7e1427b44ff34fe8fde3a88f279b
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 01:41:07 -0500

    Update 171excelSheetColumnNumber.py

--- 171excelSheetColumnNumber.py
+++ 171excelSheetColumnNumber.py
@@ -1,8 +1,5 @@
+import functools
+
 class Solution:
-    def titleToNumber(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        import functools
-        return functools.reduce(lambda acc, elem: acc * 26 + elem, (ord(char) - 64 for char in s))
+    def titleToNumber(self, s: str) -> int:
+        return functools.reduce(lambda acc, _: acc * 26 + _, (ord(char) - 64 for char in s))
