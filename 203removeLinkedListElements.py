aeed5ba9c151e2193e7d19607746058ef52727fe
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 01:49:16 -0500

    Update 203removeLinkedListElements.py

--- 203removeLinkedListElements.py
+++ 203removeLinkedListElements.py
@@ -5,15 +5,10 @@
 #         self.next = None
 
 class Solution:
-    def removeElements(self, head, val):
-        """
-        :type head: ListNode
-        :type val: int
-        :rtype: ListNode
-        """
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
         result = current = ListNode(None)
-        result.next = head       
+        result.next = head
         while current.next:
             if current.next.val == val: current.next = current.next.next
-            else: current = current.next              
+            else: current = current.next
         return result.next
