4732c90ca5e0318926992605b37d2560c099e9c2
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 01:43:30 -0500

    Update 187repeatedDNASequences.py

--- 187repeatedDNASequences.py
+++ 187repeatedDNASequences.py
@@ -1,5 +1,3 @@
-import typing
 class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> typing.List[str]:
-        import collections
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
         return [_[0] for _ in collections.Counter(s[_:_ + 10] for _ in range(len(s) - 9)).items() if _[1] > 1]
