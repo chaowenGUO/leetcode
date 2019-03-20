2ae9feb90d9a1f21e8c89b21f24244699facdcd2
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 02:12:05 -0500

    Update 343integerBreak.py

--- 343integerBreak.py
+++ 343integerBreak.py
@@ -1,7 +1,3 @@
 class Solution:
-    def integerBreak(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+    def integerBreak(self, n: int) -> int:
         return n - 1 if n < 4 else 3**((n - 2) // 3) * ((n - 2) % 3 + 2)
