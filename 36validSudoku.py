e146edfebcd141d647ede5520b9e4fea39cdec9a
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 02:07:49 -0500

    Update 36validSudoku.py

--- 36validSudoku.py
+++ 36validSudoku.py
@@ -1,5 +1,5 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         seen = ([(column, j), (i, column), (i // 3, j // 3, column)] for i, row in enumerate(board) for j, column in enumerate(row) if column != '.')
-        *seen, = itertools.chain(*seen)
+        seen = *itertools.chain(*seen),
         return len(seen) == len({*seen})
