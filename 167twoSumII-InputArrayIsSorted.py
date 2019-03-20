83749a5f8548aa21123048b9cb641e972ba1282f
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 01:40:08 -0500

    Update 167twoSumII-InputArrayIsSorted.py

--- 167twoSumII-InputArrayIsSorted.py
+++ 167twoSumII-InputArrayIsSorted.py
@@ -1,13 +1,8 @@
 class Solution:
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
-            twosum = numbers[left] + numbers[right]
-            if twosum == target: return [left + 1, right + 1]
-            elif twosum < target: left += 1
+            _ = numbers[left] + numbers[right]
+            if _ == target: return [left + 1, right + 1]
+            elif _ < target: left += 1
             else: right -= 1
