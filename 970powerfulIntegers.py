0d51a6043ab87574e6968b013358fd07f2c6da4b
chaowen guo <chaowen.guo1@gmail.com>
2019-03-21 18:34:57 -0500

    Update 970powerfulIntegers.py

--- 970powerfulIntegers.py
+++ 970powerfulIntegers.py
@@ -1,3 +1,3 @@
 class Solution:
     def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
-        return [*{x**i + y**j for i in range(18) for j in range(18) if x**i + y**j <= bound}]
+        return [*{x**i + y**j for i in range(20) for j in range(20) if x**i + y**j <= bound}]
