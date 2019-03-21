edb03140591a07133af66324306d2d732a1ca706
chaowen guo <chaowen.guo1@gmail.com>
2019-03-21 00:05:28 -0500

    Update 165compareVersionNumbers.py

--- 165compareVersionNumbers.py
+++ 165compareVersionNumbers.py
@@ -1,11 +1,5 @@
 class Solution:
-    def compareVersion(self, version1, version2):
-        """
-        :type version1: str
-        :type version2: str
-        :rtype: int
-        """
-        import itertools
+    def compareVersion(self, version1: str, version2: str) -> int:
         version = (map(int, _.split('.')) for _ in (version1, version2))
         for _ in itertools.zip_longest(*version, fillvalue = 0):
             if _[0] > _[1]: return 1
