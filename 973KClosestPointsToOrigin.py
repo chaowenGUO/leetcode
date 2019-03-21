81f2b0164c9c9bcaa35a11159d0ec5f11f7c2f06
chaowen guo <chaowen.guo1@gmail.com>
2019-03-21 18:37:38 -0500

    Update 973KClosestPointsToOrigin.py

--- 973KClosestPointsToOrigin.py
+++ 973KClosestPointsToOrigin.py
@@ -1,3 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        return heapq.nsmallest(K, points, lambda distance: distance[0]**2 + distance[1]**2)
+        return heapq.nsmallest(K, points, lambda _: _[0]**2 + _[1]**2)
