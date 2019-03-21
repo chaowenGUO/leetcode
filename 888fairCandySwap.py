5537e19b4bc362fa756eb844bf2a6435267aa1c6
chaowen guo <chaowen.guo1@gmail.com>
2019-03-21 18:18:47 -0500

    Update 888fairCandySwap.py

--- 888fairCandySwap.py
+++ 888fairCandySwap.py
@@ -1,5 +1,6 @@
 class Solution:
     def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
         diff = (sum(A) - sum(B)) // 2
+        A = {*A}
         for _ in {*B}:
-            if diff + _ in {*A}: return [_ + diff, _]
+            if diff + _ in A: return [_ + diff, _]
