30ea3a56e5e2bd9da5250662b23729f63b507b7a
chaowen guo <chaowen.guo1@gmail.com>
2019-03-20 22:45:53 -0500

    Update 119pascal'sTriangleII.py

--- 119pascal'sTriangleII.py
+++ 119pascal'sTriangleII.py
@@ -1,6 +1,6 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        result = [1] + rowIndex * [0]
+        result = [1, *itertools.repeat(0, rowIndex)]
         for i in range(1, rowIndex + 1):
             for j in range(i, 0, -1): result[j] += result[j - 1]
         return result
