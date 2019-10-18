8ce00717c7c0684583b7ddf31acd1817741c3d81
chaowen guo <chaowen.guo1@gmail.com>
2019-10-18 02:01:02 -0500

    Update 704binarySearch.py

--- 704binarySearch.py
+++ 704binarySearch.py
@@ -1,5 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left = bisect.bisect_left(nums, target)
+        return left if target in nums[left:left + 1] else -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)
         while left < right:
             middle = left + (right - left) // 2
