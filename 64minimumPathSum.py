94c4c63920c4a58f37f71e46345eb89013da1936
chaowen guo <chaowen.guo1@gmail.com>
2019-10-23 20:44:21 -0500

    Update 64minimumPathSum.py

--- 64minimumPathSum.py
+++ 64minimumPathSum.py
@@ -1,7 +1,7 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [*itertools.accumulate(grid[0])]
-        for row in range(1, len(grid)):
-            dp[0] += grid[row][0]
-            for column in range(1, len(grid[0])): dp[column] = min(dp[column], dp[column - 1]) + grid[row][column]
+        *dp, = itertools.accumulate(grid[0])
+        for row in itertools.islice(grid, 1, None):
+            dp[0] += row[0]
+            for column in range(1, len(row)): dp[column] = min(itertools.islice(dp, column - 1, column + 1)) + row[column]
         return dp[-1]
