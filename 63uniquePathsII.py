b1be11a7f46fd73ce94a41d6b9d439a5ea3d52b9
chaowen guo <chaowen.guo1@gmail.com>
2019-10-23 20:51:21 -0500

    Update 63uniquePathsII.py

--- 63uniquePathsII.py
+++ 63uniquePathsII.py
@@ -1,7 +1,7 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int: 
-        dp = [*itertools.accumulate((1 - _ for _ in obstacleGrid[0]), operator.mul)]
-        for row in range(1, len(obstacleGrid)):
-            dp[0] *= 1 - obstacleGrid[row][0]
-            for column in range(1, len(obstacleGrid[0])): dp[column] = (dp[column - 1] + dp[column]) * (1 - obstacleGrid[row][column])
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        *dp, = itertools.accumulate((1 - _ for _ in obstacleGrid[0]), operator.mul)
+        for row in itertools.islice(obstacleGrid, 1, None):
+            dp[0] *= 1 - row[0]
+            for column in range(1, len(row)): dp[column] = sum(itertools.islice(dp, column - 1, column + 1)) * (1 - row[column])
         return dp[-1]
