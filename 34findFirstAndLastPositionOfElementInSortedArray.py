b6aa8bb081ab1beb8cd5cc7501983770a3871439
chaowen guo <chaowen.guo1@gmail.com>
2019-10-23 20:14:27 -0500

    Update 34findFirstAndLastPositionOfElementInSortedArray.py

--- 34findFirstAndLastPositionOfElementInSortedArray.py
+++ 34findFirstAndLastPositionOfElementInSortedArray.py
@@ -1,23 +1,4 @@
 class Solution:
-    def lower(self, nums, target):
-        left, right = 0, len(nums) - 1
-        while left + 1 < right:
-            middle = left + (right - left) // 2
-            if nums[middle] < target: left = middle
-            else: right = middle
-        if nums[left] == target: return left
-        elif nums[right] == target: return right
-        else: return -1
-        
-    def upper(self, nums, target):
-        left, right = 0, len(nums) - 1
-        while left + 1 < right:
-            middle = left + (right - left) // 2
-            if nums[middle] <= target: left = middle
-            else: right = middle
-        if nums[right] == target: return right
-        elif nums[left] == target: return left
-        else: return -1
-        
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        return [_(nums, target) for _ in (self.lower, self.upper)] if nums else [-1] * 2
+        left = bisect.bisect_left(nums, target)
+        return [left, bisect.bisect(nums, target) - 1] if target in itertools.islice(nums, left, left + 1) else [-1] * 2
