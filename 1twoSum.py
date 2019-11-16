bd9f6683de25dfc7b45355d877c7841e0c507675
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:12:02 -0600

    Delete 1twoSum.py

--- 1twoSum.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary = {}
-        for index, num in enumerate(nums):
-            if num in dictionary: return [index, dictionary.get(num)]
-            else: dictionary.setdefault(target - num, index)
