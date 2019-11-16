8cc0acbe2fe84f02ead46665531258a02339936b
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:33:58 -0600

    Update and rename 12integerToRoman.py to 12integerToRoman/12integerToRoman.py

--- 12integerToRoman.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def intToRoman(self, num: int) -> str:
-        symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X','IX', 'V', 'IV', 'I']
-        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        result = ''
-        for symbol, value in zip(symbols, values):
-            result += symbol * (num // value)
-            num %= value
-        return result
---
+++ 12integerToRoman/12integerToRoman.py
@@ -0,0 +1,25 @@
+import enum
+
+@enum.unique
+class Roman(enum.Enum):
+    M = 1000
+    CM = 900
+    D = 500
+    CD = 400
+    C = 100
+    XC = 90
+    L = 50
+    XL = 40
+    X = 10
+    IX = 9
+    V = 5
+    IV = 4
+    I = 1
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        result = []
+        for _ in Roman:
+            result += _.name * (num // _.value),
+            num %= _.value
+        return ''.join(result)
