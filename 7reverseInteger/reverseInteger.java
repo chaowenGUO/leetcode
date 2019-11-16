ada674579277bd876dfe0700cea542e3c81b0ea0
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:18:17 -0600

    Create reverseInteger.java

---
+++ 7reverseInteger/reverseInteger.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0)
+        {
+            if (Math.abs(result) > Integer.MAX_VALUE / 10) return 0;
+            else
+            {
+                result = result * 10 + x % 10;
+                x /= 10;
+            }
+        }
+        return result;
+    }
+}
