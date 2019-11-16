e3495f4c4dbce55d31f227990ee8d5352e289da5
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 07:08:08 -0600

    Create validParentheses.js

---
+++ 20validParentheses/validParentheses.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const parentheses = Object.freeze({'{':'}', '[':']', '(':')'})
+    stack = []
+    for (const _ of s)
+    {
+        if (_ in parentheses) stack.push(parentheses[_])
+        else
+            if (!stack.length || !Object.is(stack.pop(), _)) return false 
+    }
+    return !stack.length
+};
