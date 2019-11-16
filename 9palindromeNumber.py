238695fb3fdbc7f0e1a197244a3895378354aa9d
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:26:20 -0600

    Delete 9palindromeNumber.py

--- 9palindromeNumber.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0 or x % 10 == 0 and x != 0: return False
-        else:
-            result = 0
-            while result < x:
-                result = result * 10 + x % 10
-                x //= 10
-            return result == x or result // 10 == x
