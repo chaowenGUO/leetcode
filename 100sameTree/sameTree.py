398d5a96119464685e7c47e03d786618c0dbe0b7
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 07:36:58 -0600

    Update sameTree.py

--- 100sameTree/sameTree.py
+++ 100sameTree/sameTree.py
@@ -7,5 +7,4 @@
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p and q: return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else: return p == q
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) if p and q else p == q
