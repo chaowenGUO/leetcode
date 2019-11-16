ec3e88aab5d9fa7a37252bd23d9152715b2cfe0a
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:18:49 -0600

    Create reverseInteger.cpp

---
+++ 7reverseInteger/reverseInteger.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        auto result{0};
+        while (x)
+        {
+            if (std::abs(result) > std::numeric_limits<int>::max() / 10) return false;
+            else
+            {
+                result = result * 10 + x % 10;
+                x /= 10;
+            }
+        }
+        return result;
+    }
+};
