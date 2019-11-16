6276d411a0652ff6ba9e3c79ed980a0079983c95
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:50:48 -0600

    Create mergeKSortedLists.java

---
+++ 23mergeKSortedLists/mergeKSortedLists.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        final PriorityQueue<Integer> queue = new PriorityQueue();
+        for(ListNode node: lists)
+        {
+            while (node != null)
+            {
+                queue.offer(node.val);
+                node = node.next;
+            }
+        }
+        final ListNode result = new ListNode(0);
+        ListNode current = result;
+        while (!queue.isEmpty())
+        {
+            current.next = new ListNode(queue.poll());
+            current = current.next;
+        }
+        return result.next;
+    }
+}
