2ca68948d2151c75b559e0a3e86ead65c36f2760
chaowen guo <chaowen.guo1@gmail.com>
2019-11-16 06:08:23 -0600

    Create twoSum.js

---
+++ 1twoSum/twoSum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const dictionary = new Map()
+    for (const [index, num] of nums.entries())
+    {
+        if (dictionary.has(num)) return [index, dictionary.get(num)]
+        else dictionary.set(target - num, index)
+    }
+};
