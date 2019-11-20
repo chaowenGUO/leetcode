085ef68b1889ba255ab5b06d10b339883350923c
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 05:18:17 -0600

    Update containerWithMostWater.cpp

--- 11containerWithMostWater/containerWithMostWater.cpp
+++ 11containerWithMostWater/containerWithMostWater.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        auto left{std::cbegin(height)}, right{std::prev(std::cend(height))};
+        auto left{std::cbegin(height)}, right{--std::cend(height)};
         auto result{0};
         while (left < right)
         {
             result = std::fmax(result, std::distance(left, right) * std::min(*left, *right));
-            *left < *right ? std::advance(left, 1) : std::advance(right, -1);
+            *left < *right ? ++left : --right;
         }
         return result;
     }
