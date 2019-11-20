0b16f897dd7253411cf7a5d9ffe20a8eb3221735
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 05:50:44 -0600

    Update integerToRoman.cpp

--- 12integerToRoman/integerToRoman.cpp
+++ 12integerToRoman/integerToRoman.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        std::vector const Roman{std::pair{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
+        std::vector const roman{std::pair{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
         std::vector<std::string> result;
-        std::for_each(std::cbegin(Roman), std::cend(Roman), [&num, &result](auto const _){
-            auto const [symbol, value]{_};
-            for (auto index{0}; index != num / value; ++index) result.emplace_back(symbol);
-            num %= value;});
+        for (auto const _: roman)
+        {
+            for (auto repeat{0}; repeat != num / _.second; ++repeat) result.emplace_back(_.first);
+            num %= _.second;
+        }
         std::stringstream stream;
         std::copy(std::cbegin(result), std::cend(result), std::ostream_iterator<std::string>{stream});
         return stream.str();
