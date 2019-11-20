33b2f2148b043418f0774e73cefa32c2be17e623
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 06:55:03 -0600

    Update removeDuplicatesFromSortedArray.js

--- 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
+++ 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.js
@@ -1,1 +1,14 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = nums.length ? 1 : 0
+    nums.forEach((num, _, array) => {
+        if (num > array[index - 1])
+        {
+            array[index] = num
+            ++index
+        }})
+    return index
+};
