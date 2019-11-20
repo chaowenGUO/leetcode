0e98601520396e99ff071c254deafb2cd91f3a92
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 05:21:19 -0600

    Update containerWithMostWater.py

--- 11containerWithMostWater/containerWithMostWater.py
+++ 11containerWithMostWater/containerWithMostWater.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        result = 0
         left, right = 0, len(height) - 1
+        result = 0
         while left < right:
             result = max(result, (right - left) * min(map(height.__getitem__, (left, right))))
-            if operator.sub(*map(height.__getitem__, (left, right))) < 0: left += 1
-            else: right -= 1
+            (left := left + 1) if operator.sub(*map(height.__getitem__, (left, right))) < 0 else (right := right - 1)
         return result
