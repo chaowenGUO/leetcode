ed5973c4ef6263bb400c5e952709bb18875e25b2
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 07:16:45 -0600

    Create removeElement.java

---
+++ 27removeElement/removeElement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (final int num: nums)
+        {
+            if (num != val)
+            {
+                nums[index] = num;
+                ++index;
+            }
+        }
+        return index;
+    }
+}
