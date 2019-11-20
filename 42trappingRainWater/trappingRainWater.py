c4394bb35a3600330758d412a973cb786081d591
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 11:03:08 -0600

    Update trappingRainWater.py

--- 42trappingRainWater/trappingRainWater.py
+++ 42trappingRainWater/trappingRainWater.py
@@ -1,10 +1,10 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
         result = level = 0
+        left, right = 0, len(height) - 1
         while left < right:
-            low = min(height[left], height[right])
-            (left := left + 1) if height[left] < height[right] else (right := right - 1)
+            low = min(map(height.__getitem__, (left, right)))
+            (left:= left + 1) if operator.sub(*map(height.__getitem__, (left, right))) < 0 else (right := right - 1)
             level = max(level, low)
             result += level - low
         return result
