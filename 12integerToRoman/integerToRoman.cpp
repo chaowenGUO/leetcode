752a142a164429d05f029b80746259b4546acf8a
chaowen guo <chaowen.guo1@gmail.com>
2019-11-20 05:54:49 -0600

    Update integerToRoman.cpp

--- 12integerToRoman/integerToRoman.cpp
+++ 12integerToRoman/integerToRoman.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     string intToRoman(int num) {
         std::vector const roman{std::pair{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
-        std::vector<std::string> result;
+        std::stringstream result;
         for (auto const _: roman)
         {
-            for (auto repeat{0}; repeat != num / _.second; ++repeat) result.emplace_back(_.first);
+            for (auto repeat{0}; repeat != num / _.second; ++repeat) result << _.first;
             num %= _.second;
         }
-        std::stringstream stream;
-        std::copy(std::cbegin(result), std::cend(result), std::ostream_iterator<std::string>{stream});
-        return stream.str();
+        return result.str();
     }
 };
