bda370b58627a3a86897fcbb1fc5796cec088e3b
chaowen guo <chaowen.guo1@gmail.com>
2019-11-25 15:53:22 -0600

    Update removeDuplicatesFromSortedArray.cpp

--- 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
+++ 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        auto index{std::empty(nums) ? 0 : 1};
+        std::size_t index{std::empty(nums) ? 0 : 1};
         std::for_each(std::cbegin(nums), std::cend(nums), [&](auto const num){
             if (num > nums.at(index - 1))
             {
