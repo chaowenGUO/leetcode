e0e0e033be016839d94f3d2bd67bc9a25fb67814
chaowen guo <chaowen.guo1@gmail.com>
2019-11-25 13:55:22 -0600

    Create sqrt(x).cpp

---
+++ 69sqrt(x)/sqrt(x).cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        auto [left, right]{std::pair{0, x / 2 + 1}};
+        while (left != right)
+        {
+            const auto middle{left + (right - left) / 2};
+            if (std::pow(middle, 2) < x) left = middle + 1;
+            else right = middle;
+        }
+        return std::pow(left, 2) == x? left : left - 1;
+    }
+};
