2f2c8a8d397cebb3c2c5d6ef4ea0aca57ed18296
chaowen guo <chaowen.guo1@gmail.com>
2019-11-25 16:36:02 -0600

    Update 78subsets.py

--- 78subsets.py
+++ 78subsets.py
@@ -2,4 +2,4 @@ import functools
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return functools.reduce(lambda result, num: result + [_ + [num] for _ in result], nums, [[]])
+        return functools.reduce(lambda result, num: [*result, *([*_, num] for _ in result)], nums, [[]])
