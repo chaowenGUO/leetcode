f5096019b754f675803e3b9fd47e317ac265bfbb
chaowen guo <chaowen.guo1@gmail.com>
2019-11-25 13:19:04 -0600

    Update trappingRainWater.py

--- 42trappingRainWater/trappingRainWater.py
+++ 42trappingRainWater/trappingRainWater.py
@@ -4,7 +4,7 @@ class Solution:
         left, right = 0, len(height) - 1
         while left < right:
             low = min(map(height.__getitem__, (left, right)))
-            (left:= left + 1) if operator.sub(*map(height.__getitem__, (left, right))) < 0 else (right := right - 1)
+            (left:= left + 1) if operator.lt(*map(height.__getitem__, (left, right))) else (right := right - 1)
             level = max(level, low)
             result += level - low
         return result
