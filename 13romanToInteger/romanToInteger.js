cb6383c6bee7efb2e688b61aba125af1e30fdc94
chaowen guo <chaowen.guo1@gmail.com>
2019-12-02 16:55:55 -0600

    Update romanToInteger.js

--- 13romanToInteger/romanToInteger.js
+++ 13romanToInteger/romanToInteger.js
@@ -4,5 +4,5 @@
  */
 var romanToInt = function(s) {
     const roman = Object.freeze({M:1000, D:500, C:100, L:50, X:10, V:5, I:1})
-    return [...s].reverse().reduce((result, _) => [roman[_] < roman[result[1]] ? result[0] - roman[_] : result[0] + roman[_], _], [0, 'I'])[0]
+    return [...s].reverse().reduce((result, _) => [[_, result[1]].map(_ => roman[_]).reduce((a,b) => a - b) < 0 ? result[0] - roman[_] : result[0] + roman[_], _], [0, 'I'])[0]
 };
