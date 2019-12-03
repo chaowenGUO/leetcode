3f1573d06bb7b3d2186be5759aae10f2e1f35790
chaowen guo <chaowen.guo1@gmail.com>
2019-12-02 18:03:40 -0600

    Update threeSum.java

--- 15threeSum/threeSum.java
+++ 15threeSum/threeSum.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        final ArrayList<List<Integer>> result = new ArrayList();
+        final List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         for (int index = 0; index < nums.length - 2;)
         {
             final int num = nums[index];
             if (num > 0) break;
             int left = index + 1, right = nums.length - 1;
-            while (left < right)
+            while (left != right)
             {
-                final int numsLeft = nums[left], numsRight = nums[right];
+                int numsLeft = nums[left], numsRight = nums[right];
                 final int total = num + numsLeft + numsRight;
                 if (total == 0) result.add(Arrays.asList(num, numsLeft, numsRight));
-                if (total <= 0) while (left < right && nums[left] == numsLeft) ++left;
-                if (total >= 0) while (left < right && nums[right] == numsRight) --right;
+                if (total <= 0) while (left != right && numsLeft == nums[left]) ++left;
+                if (total >= 0) while (left != right && numsRight == nums[right]) --right;
             }
-            while (num == nums[index] && index < nums.length - 2) ++index;
+            while (nums[index] == num && index < nums.length - 2) ++index;
         }
         return result;
     }
