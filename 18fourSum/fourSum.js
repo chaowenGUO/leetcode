d10aa5d9890a8163177253bb228d4083dafdf206
chaowen guo <chaowen.guo1@gmail.com>
2019-12-02 22:14:45 -0600

    Update fourSum.js

--- 18fourSum/fourSum.js
+++ 18fourSum/fourSum.js
@@ -10,14 +10,16 @@ var fourSum = function(nums, target) {
         for (let right = left + 1; !Object.is(right, nums.length); ++right)
         {
             const total = [left, right].map(_ => nums[_]).reduce((a,b) => a + b)
-            dictionary.has(total) ? dictionary.get(total).push([left, right]) : dictionary.set(total, [[left, right]])
+            dictionary.has(total) ? dictionary.get(total).add([left, right]) : dictionary.set(total, new Set([[left, right]]))
         }
+    console.log(dictionary)
     const result = new Set()
     for (let left = 2; left < nums.length - 1; ++left)
         for (let right = left + 1; !Object.is(right, nums.length); ++right)
         {
             const complement = target - [left, right].map(_ => nums[_]).reduce((a,b) => a + b)
-            if (dictionary.has(complement)) dictionary.get(complement).filter(_ => _[1] < left).forEach(_ => result.add(JSON.stringify([..._, left, right].map(_ => nums[_]))))
+            if (dictionary.has(complement)) dictionary.get(complement).forEach(_ => {
+                if (_[1] < left) result.add(JSON.stringify([..._, left, right].map(_ => nums[_])))})
         }
     return Array.from(result, JSON.parse)
 };
