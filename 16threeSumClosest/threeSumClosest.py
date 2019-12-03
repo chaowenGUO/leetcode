e6048288f5a6c00f1f970f28443d59630fdbf312
chaowen guo <chaowen.guo1@gmail.com>
2019-12-02 18:52:28 -0600

    Update threeSumClosest.py

--- 16threeSumClosest/threeSumClosest.py
+++ 16threeSumClosest/threeSumClosest.py
@@ -6,14 +6,14 @@ class Solution:
         while index != len(nums) - 2:
             num = nums[index]
             left, right = index + 1, len(nums) - 1
-            while left < right:
+            while left != right:
                 numsLeft, numsRight = map(nums.__getitem__, (left, right))
                 total = num + numsLeft + numsRight
-                if total == target: return target 
+                if total == target: return target
                 if total <= target:
-                    while left < right and nums[left] == numsLeft: left += 1
+                    while left != right and nums[left] == numsLeft: left += 1
                 if total >= target:
-                    while left < right and nums[right] == numsRight: right -= 1                             
-                if abs(target-total) < abs(result - target): result = total           
-            while index != len(nums) - 2 and nums[index] == num: index += 1
+                    while left != right and nums[right] == numsRight: right -= 1
+                if abs(total - target) < abs(result - target): result = total
+            while num == nums[index] and index != len(nums) - 2: index += 1
         return result
