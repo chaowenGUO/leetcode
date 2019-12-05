f71b9e27d1cbfc722fc277a83b46711cc751cc42
chaowen guo <chaowen.guo1@gmail.com>
2019-12-05 15:20:19 -0600

    Update 56mergeIntervals.py

--- 56mergeIntervals.py
+++ 56mergeIntervals.py
@@ -1,13 +1,7 @@
-# Definition for an interval.
-# class Interval:
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution:
-    def merge(self, intervals: List[Interval]) -> List[Interval]:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         result = []
-        for interval in sorted(intervals, key = lambda _: _.start):
-            if result and interval.start <= result[-1].end: result[-1].end = max(result[-1].end, interval.end)
+        for interval in sorted(intervals, key = lambda _:_[0]):
+            if result and interval[0] <= result[-1][1]: result[-1][1] = max(result[-1][1], interval[1])
             else: result += interval,
         return result
