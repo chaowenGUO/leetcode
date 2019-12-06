bf11ebe6058de57bfe3091be433a30f1b00cdd00
chaowen guo <chaowen.guo1@gmail.com>
2019-12-05 19:11:14 -0600

    Update removeDuplicatesFromSortedArray.java

--- 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.java
+++ 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.java
@@ -2,13 +2,11 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int index = nums.length != 0 ? 1 : 0;
         for (final int num: nums)
-        {
             if (num > nums[index - 1])
             {
                 nums[index] = num;
                 ++index;
             }
-        }
         return index;
     }
 }
