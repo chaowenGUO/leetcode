5a9c8edfd6b00f97a11c79ea775bb9d8f34a4c8c
chaowen guo <chaowen.guo1@gmail.com>
2019-12-05 19:54:27 -0600

    Update validParentheses.cpp

--- 20validParentheses/validParentheses.cpp
+++ 20validParentheses/validParentheses.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isValid(string s) {
-        std::unordered_map const parentheses{std::pair{'(', ')'}, {'[', ']'}, {'{', '}'}};
+        std::unordered_map const parentheses{std::pair{'{','}'}, {'[',']'}, {'(',')'}};
         std::stack<char> stack;
         for (auto const _: s)
-        {
-            if (auto find{parentheses.find(_)}; find != std::cend(parentheses)) stack.emplace(parentheses.at(_));
+            if (auto find{parentheses.find(_)}; find != std::cend(parentheses)) stack.emplace(find->second);
             else
-            {
-                if (stack.empty() || stack.top() != _) return false;
+            {    
+                if (std::empty(stack) || stack.top() != _) return false;
                 else stack.pop();
             }
-        }
-        return stack.empty();
+        return std::empty(stack);
     }
 };
