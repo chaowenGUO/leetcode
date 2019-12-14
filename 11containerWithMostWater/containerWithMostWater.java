290bd9c6957afcd7d65750d862b40838275e9610
chaowen guo <chaowen.guo1@gmail.com>
2019-12-14 16:34:12 -0600

    Update containerWithMostWater.java

--- 11containerWithMostWater/containerWithMostWater.java
+++ 11containerWithMostWater/containerWithMostWater.java
@@ -2,10 +2,10 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
         int result = 0;
-        while (left < right)
+        while (left != right)
         {
-            result = Math.max(result, (right - left) * java.util.stream.IntStream.of(left, right).map($ -> height[$]).min().orElse(0));
-            int $_ = java.util.stream.IntStream.of(left, right).map($ -> height[$]).reduce((a, b) -> a - b).orElse(0) < 0 ? ++left : --right;
+            result = Math.max(result, (right - left) * IntStream.of(left, right).map($ -> height[$]).min().orElse(0));
+            int $$ = IntStream.of(left, right).map($ -> height[$]).reduce((a,b) -> a - b).orElse(0) < 0 ? ++left : --right;
         }
         return result;
     }
