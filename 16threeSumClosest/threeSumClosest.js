d5d36b3015bb070d6af8d2f110dd936fda9b4ea0
chaowen guo <chaowen.guo1@gmail.com>
2019-12-15 00:24:16 -0600

    Update threeSumClosest.js

--- 16threeSumClosest/threeSumClosest.js
+++ 16threeSumClosest/threeSumClosest.js
@@ -5,8 +5,8 @@
  */
 var threeSumClosest = function(nums, target) {
     nums.sort((a,b) => a - b)
-    let result = Array.from({length:3}, (_, index) => nums[index]).reduce((a,b) => a + b)
-    for (let index = 0; index != nums.length - 2;)
+    let result = nums.slice(0, 3).reduce((a, b) => a + b)
+    for (let index = 0; !Object.is(index, nums.length - 2);)
     {
         const num = nums[index]
         let [left, right] = [index + 1, nums.length - 1]
@@ -15,11 +15,11 @@ var threeSumClosest = function(nums, target) {
             const [numsLeft, numsRight] = [left, right].map(_ => nums[_])
             const total = num + numsLeft + numsRight
             if (Object.is(total, target)) return target
-            if (total <= target) while (!Object.is(left, right) && Object.is(numsLeft, nums[left])) ++left
-            if (total >= target) while (!Object.is(left, right) && Object.is(numsRight, nums[right])) --right
+            if (total <= target) while (!Object.is(left, right) && Object.is(nums[left], numsLeft)) ++left
+            if (total >= target) while (!Object.is(left, right) && Object.is(nums[right], numsRight)) --right
             if (Math.abs(total - target) < Math.abs(result - target)) result = total
         }
-        while (Object.is(nums[index], num) && index != nums.length - 2) ++index
+        while (Object.is(nums[index], num) && !Object.is(index, nums.length - 2)) ++index
     }
     return result
 };
