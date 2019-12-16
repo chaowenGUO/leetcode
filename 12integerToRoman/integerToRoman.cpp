088dfba3c969ebcd7a8e4883b78b81383a179162
chaowen guo <chaowen.guo1@gmail.com>
2019-12-16 01:17:36 -0600

    Update integerToRoman.cpp

--- 12integerToRoman/integerToRoman.cpp
+++ 12integerToRoman/integerToRoman.cpp
@@ -3,11 +3,10 @@ public:
     string intToRoman(int num) {
         std::vector const roman{std::pair{"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400}, {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40}, {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}};
         std::stringstream result;
-        for (auto const _: roman)
-        {
-            for (auto repeat{0}; repeat != num / _.second; ++repeat) result << _.first;
-            num %= _.second;
-        }
+        std::for_each(std::cbegin(roman), std::cend(roman), [&](auto const _){
+            auto const [symbol, value]{_};
+            for (std::size_t _{}; _ != num / value; ++_) result << symbol;
+            num %= value;});
         return result.str();
     }
 };
