7de1cd4d6fce2ac2d95a4aa4b993bde3689d55e2
chaowen guo <chaowen.guo1@gmail.com>
2019-12-16 20:57:21 -0600

    Update 60permutationSequence.py

--- 60permutationSequence.py
+++ 60permutationSequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         factorial = math.factorial(n)
-        result, k = '', (k - 1) % factorial
-        *element, = range(1, n + 1)
-        while element:
-            factorial //= len(element)
+        result, k = [], k - 1
+        *elements, = range(1, n + 1)
+        while elements:
+            factorial //= len(elements)
             _, k = divmod(k, factorial)
-            result += str(element.pop(_))
-        return result
+            result += elements.pop(_),
+        return ''.join(map(str, result))
