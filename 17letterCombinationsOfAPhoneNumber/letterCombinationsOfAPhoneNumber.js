eac4631220ca3ba142757a4278f8e0fc74836114
chaowen guo <chaowen.guo1@gmail.com>
2019-12-22 01:59:55 -0600

    Update letterCombinationsOfAPhoneNumber.js

--- 17letterCombinationsOfAPhoneNumber/letterCombinationsOfAPhoneNumber.js
+++ 17letterCombinationsOfAPhoneNumber/letterCombinationsOfAPhoneNumber.js
@@ -2,9 +2,10 @@
  * @param {string} digits
  * @return {string[]}
  */
+
 const lodash = require('lodash')
 
 var letterCombinations = function(digits) {
-    const phone = Object.freeze({2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'});
-    return digits.length ? [...digits].reduce((result, digit) => lodash.flatMap(result, x => Array.from(phone[digit], y => x + y)), ['']) : []
+    const phone = Object.freeze({2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'})
+    return digits ? [...digits].reduce((result, digit) => lodash.flatMap(result, x => Array.from(phone[digit], y => x + y)), ['']) : []
 };
