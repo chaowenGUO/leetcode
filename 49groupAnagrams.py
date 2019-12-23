a6266e7b9843ae5de7c6e248fa55d5b7de38a4a3
chaowen guo <chaowen.guo1@gmail.com>
2019-12-23 02:37:00 -0600

    Update 49groupAnagrams.py

--- 49groupAnagrams.py
+++ 49groupAnagrams.py
@@ -1,5 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dictionary = collections.defaultdict(list)
-        for string in strs: dictionary[frozenset(collections.Counter(string).items())] += string,
+        for _ in strs: dictionary[frozenset(collections.Counter(_).items())] += _,
         return [*dictionary.values()]
