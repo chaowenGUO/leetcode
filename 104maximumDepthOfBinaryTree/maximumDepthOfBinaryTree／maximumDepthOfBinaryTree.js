efa7087aef00825b2dc11960900b814517aff3c6
chaowen guo <chaowen.guo1@gmail.com>
2019-12-24 02:23:19 -0600

    Create maximumDepthOfBinaryTree／maximumDepthOfBinaryTree.js

---
+++ 104maximumDepthOfBinaryTree/maximumDepthOfBinaryTree／maximumDepthOfBinaryTree.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    return root ? Math.max(...[root.left, root.right].map(maxDepth)) + 1 : 0
+};
