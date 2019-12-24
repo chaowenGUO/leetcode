e69333f7f93e694631fe3121e53ff4a83967adec
chaowen guo <chaowen.guo1@gmail.com>
2019-12-23 23:09:39 -0600

    Update climbingStairs.cpp

--- 70climbingStairs/climbingStairs.cpp
+++ 70climbingStairs/climbingStairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        std::array result{1, 1};
-        for (std::size_t _{0}; _ != n; ++_) result = {result.back(), result.front() + result.back()};
+        std::array result{1l, 1l};
+        for (std::size_t _{}; _ != n; ++_) result = {result.back(), result.front() + result.back()};
         return result.front();
     }
 };
