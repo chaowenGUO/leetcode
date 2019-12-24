409a5ef2b02d27e0786e05a0d2038707224b1f9f
chaowen guo <chaowen.guo1@gmail.com>
2019-12-24 03:14:07 -0600

    Update 137singleNumberII.py

--- 137singleNumberII.py
+++ 137singleNumberII.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        a = b = 0
+        a, b = 0, 0
         for num in nums:
-            a = (a ^ num) | b
-            b = (b ^ num) & a
+            b = (b ^ num) & ~a
+            a = (a ^ num) & ~b
         return b
