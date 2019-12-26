d23b5d7b4a2033338b25ee80441f6dc106f31718
chaowen guo <chaowen.guo1@gmail.com>
2019-12-26 03:31:15 -0600

    Update reverseInteger.js

--- 7reverseInteger/reverseInteger.js
+++ 7reverseInteger/reverseInteger.js
@@ -5,13 +5,11 @@
 var reverse = function(x) {
     let result = 0
     while (x)
-    {
         if (Math.abs(result) > ((1n << 31n) - 1n) / 10n) return 0
         else
         {
             result = result * 10 + x % 10
             x = Math.trunc(x / 10)
         }
-    }
     return result
 };
