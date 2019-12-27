fe39d6c3b23081454dbbaa4f673fd7cdf099b3eb
chaowen guo <chaowen.guo1@gmail.com>
2019-12-26 23:45:51 -0600

    Create findNumbersWithEvenNumberOfDigits.js

---
+++ 1295findNumbersWithEvenNumberOfDigits/findNumbersWithEvenNumberOfDigits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function even(num)
+{
+    let result = 0
+    while (num)
+    {
+        num = Math.trunc(num / 10)
+        result += 1
+    }
+    return 1 - result & 1
+}
+
+var findNumbers = function(nums) {
+    return nums.map(even).reduce((a,b) => a + b)
+};
