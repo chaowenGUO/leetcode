0fc87a62d4c7652a347007ef5280e005aaf8715d
chaowen guo <chaowen.guo1@gmail.com>
2019-12-26 23:51:55 -0600

    Create findNumbersWithEvenNumberOfDigits.java

---
+++ 1295findNumbersWithEvenNumberOfDigits/findNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,16 @@
+class Solution {
+    private static int even(int num)
+    {
+        int result = 0;
+        while (num != 0)
+        {
+            num /= 10;
+            result += 1;
+        }
+        return 1 - result & 1;
+    }
+    
+    public int findNumbers(int[] nums) {
+        return Arrays.stream(nums).map(Solution::even).sum();
+    }
+}
