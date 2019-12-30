ca6c5e495fe9bf429fa99496714c1fcbf6b425c5
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 09:44:04 -0600

    Update deepestLeavesSum.cpp

--- 1302deepestLeavesSum/deepestLeavesSum.cpp
+++ 1302deepestLeavesSum/deepestLeavesSum.cpp
@@ -14,9 +14,8 @@ public:
         std::vector level{root};
         while (root && !std::empty(level))
         {
-            std::vector<decltype(std::declval<TreeNode>().val)> value(std::size(level));
-            std::transform(std::cbegin(level), std::cend(level), std::begin(value), [](auto const node){return node->val;});
-            result = std::move(value);
+            result.clear();
+            std::transform(std::cbegin(level), std::cend(level), std::back_inserter(result), [](auto const node){return node->val;});
             std::vector<decltype(root)> nodes;
             std::for_each(std::cbegin(level), std::cend(level), [&](auto const node){
                 if (node->left) nodes.emplace_back(node->left);
