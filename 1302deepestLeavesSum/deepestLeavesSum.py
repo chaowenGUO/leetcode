9d7337962b88a59b6543c96b4c197a639aa0813b
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 07:10:18 -0600

    Create deepestLeavesSum.py

---
+++ 1302deepestLeavesSum/deepestLeavesSum.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        result, level = [], [root]
+        while root and level:
+            result = (node.val for node in level)
+            level = [child for node in level for child in (node.left, node.right) if child]
+        return sum(result)
