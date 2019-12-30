23ef2d1dfe24ad12ddb2f864d70ce36efe99b512
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 07:24:13 -0600

    Create deepestLeavesSum.java

---
+++ 1302deepestLeavesSum/deepestLeavesSum.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        List<Integer> result = Collections.emptyList();
+        List<TreeNode> level = Arrays.asList(root);
+        while (root != null && !level.isEmpty())
+        {
+            result = level.stream().map(node -> node.val).collect(Collectors.toList());
+            level = level.stream().flatMap(node -> Stream.of(node.left, node.right).filter($ -> $ != null)).collect(Collectors.toList());
+        }
+        return result.stream().mapToInt(Integer::intValue).sum();
+    }
+}
