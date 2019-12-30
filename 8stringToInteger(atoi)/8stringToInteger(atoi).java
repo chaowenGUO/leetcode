90d3a1162535f85d5da900075ddbac8f13f04249
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 04:25:37 -0600

    Update 8stringToInteger(atoi).java

--- 8stringToInteger(atoi)/8stringToInteger(atoi).java
+++ 8stringToInteger(atoi)/8stringToInteger(atoi).java
@@ -1,7 +1,7 @@
 class Solution {
     public int myAtoi(String str) {
         final java.util.regex.Matcher matcher = java.util.regex.Pattern.compile("^[+-]?\\d+").matcher(str.trim());
-        final java.math.BigInteger result = new java.math.BigInteger(matcher.find() ? matcher.group(0) : "0");
+        final java.math.BigInteger result = new java.math.BigInteger(matcher.find() ? matcher.group() : "0");
         return Stream.of(result, java.math.BigInteger.valueOf(Integer.MAX_VALUE), java.math.BigInteger.valueOf(Integer.MIN_VALUE)).sorted().skip(1).findFirst().orElse(java.math.BigInteger.ZERO).intValue();
     }
 }
