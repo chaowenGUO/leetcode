ed9f5b5429ada4cb9abd1ee62dd11db351ec0f46
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 23:11:22 -0600

    Update subsetsII.js

--- 90subsetsII/subsetsII.js
+++ 90subsetsII/subsetsII.js
@@ -6,5 +6,5 @@
 const lodash = require('lodash')
 
 var subsetsWithDup = function(nums) {
-    return nums.sort((a,b) => a - b).reduce((result, num) => [...result, ...result.map(_ => [..._, num]).filter(_ => !result.some($ => lodash.isEqual($, _)))], [[]])
+    return nums.sort((a,b) => a - b).reduce((result, num) => lodash.uniqWith([...result, ...result.map(_ => [..._, num])], lodash.isEqual), [[]])
 };
