d6dcf4fee5355012aacf70301238444320be9c03
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 23:48:42 -0600

    Update fourSum.js

--- 18fourSum/fourSum.js
+++ 18fourSum/fourSum.js
@@ -3,22 +3,18 @@
  * @param {number} target
  * @return {number[][]}
  */
+
+const lodash = require('lodash')
+
 var fourSum = function(nums, target) {
     nums.sort((a,b) => a - b)
-    const dictionary = new Map()
-    for (let left = 0; left < nums.length - 1; ++left)
-        for (let right = left + 1; !Object.is(right, nums.length); ++right)
-        {
-            const total = [left, right].map(_ => nums[_]).reduce((a,b) => a + b)
-            dictionary.has(total) ? dictionary.get(total).add([left, right]) : dictionary.set(total, new Set([[left, right]]))
-        }
-    console.log(dictionary)
+    const dictionary = lodash.groupBy(lodash.flatMap(lodash.range(nums.length - 1), left => lodash.range(left + 1, nums.length).map(right => [left, right])), _ => _.map(_ => nums[_]).reduce((a,b) => a + b))    
     const result = new Set()
     for (let left = 2; left < nums.length - 1; ++left)
         for (let right = left + 1; !Object.is(right, nums.length); ++right)
         {
             const complement = target - [left, right].map(_ => nums[_]).reduce((a,b) => a + b)
-            if (dictionary.has(complement)) dictionary.get(complement).forEach(_ => {
+            if (complement in dictionary) dictionary[complement].forEach(_ => {
                 if (_[1] < left) result.add(JSON.stringify([..._, left, right].map(_ => nums[_])))})
         }
     return Array.from(result, JSON.parse)
