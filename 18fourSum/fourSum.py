2a578b5da2ebfaae5c33bd2450e13f34230d81ed
chaowen guo <chaowen.guo1@gmail.com>
2019-12-31 07:47:30 -0600

    Update fourSum.py

--- 18fourSum/fourSum.py
+++ 18fourSum/fourSum.py
@@ -1,9 +1,7 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()        
-        dictionary = {key:{*group} for key, group in itertools.groupby(sorted(filter(lambda _: operator.lt(*_), itertools.product(range(len(nums)), repeat = 2)), key = lambda _: sum(map(nums.__getitem__, _))), lambda _: sum(map(nums.__getitem__, _)))}
-        result = set()
-        for left in range(2, len(nums) - 1):
-            for right in range(left + 1, len(nums)):
-                if (complement := target - sum(map(nums.__getitem__, (left, right)))) in dictionary: result.update(tuple(map(nums.__getitem__, (*_, left, right)))  for _ in dictionary.get(complement) if _[1] < left)
+        dictionary = {key:{*group} for key, group in itertools.groupby(sorted((_ for _ in itertools.product(range(len(nums)), repeat = 2) if operator.lt(*_)), key = lambda _: sum(map(nums.__getitem__, _))), lambda _: sum(map(nums.__getitem__, _)))}
+        result = {frozenset(dictionary.get(key, ())):{*group} for key, group in itertools.groupby(sorted((_ for _ in itertools.product(range(len(nums)), repeat = 2) if operator.lt(*_) and _[0] != 0 and _[0] != 1), key = lambda _: target - sum(map(nums.__getitem__, _))), lambda _: target - sum(map(nums.__getitem__, _)))}
+        result = {*map(lambda _: tuple(map(nums.__getitem__, _)), {tuple(itertools.chain(*_)) for _ in itertools.chain(*itertools.starmap(itertools.product, result.items())) if _[0][1] < _[1][0]})}
         return [[*_] for _ in result]
