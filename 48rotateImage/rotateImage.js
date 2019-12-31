07f8c741b72aa9915c3db222a17db40982de0b23
chaowen guo <chaowen.guo1@gmail.com>
2019-12-31 03:18:29 -0600

    Update rotateImage.js

--- 48rotateImage/rotateImage.js
+++ 48rotateImage/rotateImage.js
@@ -4,6 +4,6 @@
  */
 var rotate = function(matrix) {
     matrix.reverse()
-    for (let row = 0; !Object.is(row, matrix.length); ++row)
-        for (let column = row; !Object.is(column, matrix.length); ++column) [matrix[row][column], matrix[column][row]] = [matrix[column][row], matrix[row][column]]
+    for (const row of Array(matrix.length).keys())
+        for (let column = row + 1; !Object.is(column, matrix.length); ++column) [matrix[row][column], matrix[column][row]] = [matrix[column][row], matrix[row][column]]
 };
