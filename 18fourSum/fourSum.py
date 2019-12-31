28ca9c5a7027a08e0837a994cc2ea75f52d152ce
chaowen guo <chaowen.guo1@gmail.com>
2019-12-30 23:12:58 -0600

    Update fourSum.py

--- 18fourSum/fourSum.py
+++ 18fourSum/fourSum.py
@@ -1,11 +1,9 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
-        dictionary = collections.defaultdict(set)
-        for left in range(len(nums) - 1):
-            for right in range(left + 1, len(nums)): dictionary[sum(map(nums.__getitem__, (left, right)))].add((left, right))
+        nums.sort()        
+        dictionary = {key:{*group} for key, group in itertools.groupby(sorted(filter(lambda _: operator.lt(*_), itertools.product(range(len(nums)), repeat = 2)), key = lambda _: sum(map(nums.__getitem__, _))), lambda _: sum(map(nums.__getitem__, _)))}
         result = set()
         for left in range(2, len(nums) - 1):
             for right in range(left + 1, len(nums)):
-                if (complement := target - sum(map(nums.__getitem__, (left, right)))) in dictionary: result.update(tuple(map(nums.__getitem__, (*_, left, right))) for _ in dictionary.get(complement) if _[1] < left)
+                if (complement := target - sum(map(nums.__getitem__, (left, right)))) in dictionary: result.update(tuple(map(nums.__getitem__, (*_, left, right)))  for _ in dictionary.get(complement) if _[1] < left)
         return [[*_] for _ in result]
