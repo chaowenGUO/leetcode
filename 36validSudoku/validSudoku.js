e143c4d2bdbf32ee57782a517a57c8a9ddce6506
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 07:54:08 -0600

    Update validSudoku.js

--- 36validSudoku/validSudoku.js
+++ 36validSudoku/validSudoku.js
@@ -5,6 +5,6 @@
 const lodash = require('lodash')
 
 var isValidSudoku = function(board) {
-    const seen = lodash.flatten(lodash.flatMap(board, (row, i) => row.map((column, j) => [[column, j], [i, column], [Math.trunc(i / 3), Math.trunc(j / 3), column]].filter(_ => !_.includes('.')))))
+    const seen = lodash.flatMap(board, (row, i) => lodash.flatMap(row, (column, j) => [[column, j], [i, column], [Math.trunc(i / 3), Math.trunc(j / 3), column]].filter(_ => !_.includes('.'))))
     return Object.is(seen.length, lodash.uniqWith(seen, lodash.isEqual).length)
 };
