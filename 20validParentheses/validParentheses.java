ca90c87835a8dd738a4c05ee3e245a59cbe9bc4e
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 06:10:17 -0600

    Update validParentheses.java

--- 20validParentheses/validParentheses.java
+++ 20validParentheses/validParentheses.java
@@ -6,11 +6,8 @@ class Solution {
         parentheses.putIfAbsent("{", "}");
         final LinkedList<String> stack = new LinkedList<>();
         for (final String $: s.codePoints().mapToObj($ -> String.format("%c", $)).collect(Collectors.toList()))
-        {
             if (parentheses.containsKey($)) stack.push(parentheses.get($));
-            else
-                if (stack.isEmpty() || !stack.pop().equals($)) return false;
-        }
+            else if (stack.isEmpty() || !stack.pop().equals($)) return false;
         return stack.isEmpty();
     }
 }
