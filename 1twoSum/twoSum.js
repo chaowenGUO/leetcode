1602fa092a871775cc43058f4eb8c62fc30075c2
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 03:16:49 -0600

    Update twoSum.js

--- 1twoSum/twoSum.js
+++ 1twoSum/twoSum.js
@@ -6,8 +6,6 @@
 var twoSum = function(nums, target) {
     const dictionary = new Map()
     for (const [index, num] of nums.entries())
-    {
         if (dictionary.has(num)) return [index, dictionary.get(num)]
         else dictionary.set(target - num, index)
-    }
 };
