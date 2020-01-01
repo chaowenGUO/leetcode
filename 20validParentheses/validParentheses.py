3f5fb4798e1663e801a6a4a5ee71fc3dc7352414
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 06:09:21 -0600

    Update validParentheses.py

--- 20validParentheses/validParentheses.py
+++ 20validParentheses/validParentheses.py
@@ -4,6 +4,5 @@ class Solution:
         stack = []
         for _ in s:
             if _ in parentheses: stack += parentheses.get(_),
-            else:
-                if not stack or stack.pop() != _: return False
+            elif not stack or stack.pop() != _: return False
         return not stack
