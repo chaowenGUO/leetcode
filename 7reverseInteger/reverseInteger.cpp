2c3892b211bcf0999312cf8c9fff4dc7b500363d
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 03:44:43 -0600

    Update reverseInteger.cpp

--- 7reverseInteger/reverseInteger.cpp
+++ 7reverseInteger/reverseInteger.cpp
@@ -3,7 +3,7 @@ public:
     int reverse(int x) {
         auto result{0};
         while (x)
-            if (std::abs(result) > std::numeric_limits<int>::max() / 10) return false;
+            if (std::abs(result) > std::numeric_limits<decltype(x)>::max() / 10) return 0;
             else
             {
                 result = result * 10 + x % 10;
