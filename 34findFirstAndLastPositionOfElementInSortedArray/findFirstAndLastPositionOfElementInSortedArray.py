6e12f8f51217308b4f3ae77880cdb42165a5a105
chaowen guo <chaowen.guo1@gmail.com>
2020-01-01 06:52:08 -0600

    Update findFirstAndLastPositionOfElementInSortedArray.py

--- 34findFirstAndLastPositionOfElementInSortedArray/findFirstAndLastPositionOfElementInSortedArray.py
+++ 34findFirstAndLastPositionOfElementInSortedArray/findFirstAndLastPositionOfElementInSortedArray.py
@@ -1,4 +1,4 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left, right = map(lambda _:_(nums, target), (bisect.bisect_left, bisect.bisect))
+        left, right = (_(nums, target) for _ in (bisect.bisect_left, bisect.bisect))
         return [left, right - 1] if left != right else [-1] * 2
