12e3b51446740cc83d45d0c4f1eace311967f4dc
chaowen guo <chaowen.guo1@gmail.com>
2020-01-02 01:04:09 -0600

    Update fourSum.js

--- 18fourSum/fourSum.js
+++ 18fourSum/fourSum.js
@@ -8,14 +8,6 @@ const lodash = require('lodash')
 
 var fourSum = function(nums, target) {
     nums.sort((a,b) => a - b)
-    const dictionary = lodash.groupBy(lodash.flatMap(lodash.range(nums.length - 1), left => lodash.range(left + 1, nums.length).map(right => [left, right])), _ => _.map(_ => nums[_]).reduce((a,b) => a + b))    
-    const result = []
-    for (let left = 2; left < nums.length - 1; ++left)
-        for (let right = left + 1; !Object.is(right, nums.length); ++right)
-        {
-            const complement = target - [left, right].map(_ => nums[_]).reduce((a,b) => a + b)
-            if (complement in dictionary) dictionary[complement].forEach(_ => {
-                if (_[1] < left) result.push([..._, left, right].map(_ => nums[_]))})
-        }
-    return lodash.uniqWith(result, lodash.isEqual)
+    const dictionary = lodash.groupBy(lodash.flatten(Array.from({length: nums.length - 1}, (_, left) => lodash.range(left + 1, nums.length).map(right => [left, right]))), _ => _.map(_ => nums[_]).reduce((a,b) => a + b))
+    return lodash.uniqWith(lodash.flatMap(Object.entries(lodash.groupBy(lodash.flatMap(lodash.range(2, nums.length - 1), left => lodash.range(left + 1, nums.length).map(right => [left, right])), _ => target - _.map(_ => nums[_]).reduce((a,b) => a + b))), ([key, value]) => lodash.flatMap(dictionary[key] || [], x => value.map(y => [...x, ...y])).filter(_ => _[1] < _[2]).map(_ => _.map(_ => nums[_]))), lodash.isEqual)
 };
