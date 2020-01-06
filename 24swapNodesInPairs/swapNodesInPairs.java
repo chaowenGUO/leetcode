aeef1d01452b54b3313eb535939894056629ca09
chaowen guo <chaowen.guo1@gmail.com>
2020-01-06 10:59:41 -0600

    Update swapNodesInPairs.java

--- 24swapNodesInPairs/swapNodesInPairs.java
+++ 24swapNodesInPairs/swapNodesInPairs.java
@@ -1,1 +1,24 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        final ListNode result = new ListNode(0);
+        ListNode current = result;
+        current.next = head;
+        ListNode a;
+        while ((a = current.next) != null && current.next.next != null)
+        {
+            current.next = current.next.next;
+            a.next = a.next.next;
+            current.next.next = a;
+            current = a;
+        }
+    return result.next;
+    }
+}
