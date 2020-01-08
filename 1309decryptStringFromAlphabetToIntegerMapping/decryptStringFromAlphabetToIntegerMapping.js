1f94c438b35706951aa3bb9647ee57bbe291f455
chaowen guo <chaowen.guo1@gmail.com>
2020-01-08 12:02:59 -0600

    Update decryptStringFromAlphabetToIntegerMapping.js

--- 1309decryptStringFromAlphabetToIntegerMapping/decryptStringFromAlphabetToIntegerMapping.js
+++ 1309decryptStringFromAlphabetToIntegerMapping/decryptStringFromAlphabetToIntegerMapping.js
@@ -3,5 +3,5 @@
  * @return {string}
  */
 var freqAlphabets = function(s) {
-    return Array.from(s.matchAll(/\d{2}#|\d/g), _ => String.fromCodePoint(Number(_[0].replace('#', '')) +  'a'.codePointAt(0) - 1)).join('')
+    return Array.from(s.matchAll(/\d{2}#|\d/g), group => String.fromCodePoint(Number(group[0].replace('#', '')) +  'a'.codePointAt(0) - 1)).join('')
 };
