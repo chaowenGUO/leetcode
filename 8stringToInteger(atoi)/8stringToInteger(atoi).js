13449a1e2f3b3d36aa7a649c01d9ce2cbd686b19
chaowen guo <chaowen.guo1@gmail.com>
2020-01-08 10:20:24 -0600

    Update 8stringToInteger(atoi).js

--- 8stringToInteger(atoi)/8stringToInteger(atoi).js
+++ 8stringToInteger(atoi)/8stringToInteger(atoi).js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var myAtoi = function(str) {
-    let result = /^[+-]?\d+/.exec(str.trim())
+    let result = str.trim().match(/^[+-]?\d+/)
     result = BigInt(result ? result[0] : 0)
-    return [result, -1n << 31n, (1n << 31n) - 1n].sort((a, b) => a - b)[1]
+    return [result, -1n << 31n, (1n << 31n) - 1n].sort((a,b) => a - b)[1]
 };
