bf88830f93baf9b1dbc3ed02a36ac458676a5a9f
chaowen guo <chaowen.guo1@gmail.com>
2020-01-11 11:44:46 -0600

    Create 63uniquePathsII.java

---
+++ 63uniquePathsII/63uniquePathsII.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int[] dp = Arrays.stream(obstacleGrid[0]).map($ -> 1 - $).toArray();
+        Arrays.parallelPrefix(dp, (a,b) -> a * b);
+        Stream.of(obstacleGrid).skip(1).forEach(row -> {
+            dp[0] *= 1 - row[0];
+            IntStream.range(1, row.length).forEach(column -> dp[column] = IntStream.range(column - 1, column + 1).map($ -> dp[$]).sum() * (1 - row[column]));});
+        return dp[dp.length - 1];   
+    }
+}
