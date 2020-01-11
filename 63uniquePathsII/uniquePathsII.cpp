b5139c261f483ef72d587bc495be143f8692d44f
chaowen guo <chaowen.guo1@gmail.com>
2020-01-11 13:02:38 -0600

    Update uniquePathsII.cpp

--- 63uniquePathsII/uniquePathsII.cpp
+++ 63uniquePathsII/uniquePathsII.cpp
@@ -4,9 +4,9 @@ public:
         std::vector<unsigned> dp(std::cbegin(obstacleGrid.front()), std::cend(obstacleGrid.front()));
         std::transform(std::cbegin(dp), std::cend(dp), std::begin(dp), [](auto const _){return 1 - _;});
         std::partial_sum(std::cbegin(dp), std::cend(dp), std::begin(dp), std::multiplies{});
-        std::for_each(std::next(std::cbegin(obstacleGrid)), std::cend(obstacleGrid), [&dp](auto const row){
+        std::for_each(++std::cbegin(obstacleGrid), std::cend(obstacleGrid), [&dp](auto const row){
             dp.front() *= 1 - row.front();
-            for (std::size_t column{1}; column != std::size(row); ++column) dp.at(column) = (dp.at(column - 1) + dp.at(column)) * (1 - row.at(column));});
+            for (std::size_t column{1}; column != std::size(row); ++column) dp.at(column) = (dp.at(column) + dp.at(column - 1)) * (1 - row.at(column));});
         return dp.back();
     }
 };
