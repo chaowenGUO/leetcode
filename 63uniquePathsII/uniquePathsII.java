5157dd922c49cca457eab5c8ff4ff115592d42e6
chaowen guo <chaowen.guo1@gmail.com>
2020-01-11 17:20:42 -0600

    Update uniquePathsII.java

--- 63uniquePathsII/uniquePathsII.java
+++ 63uniquePathsII/uniquePathsII.java
@@ -1,10 +1,11 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int[] dp = Arrays.stream(obstacleGrid[0]).map($ -> 1 - $).toArray();
-        Arrays.parallelPrefix(dp, (a,b) -> a * b);
+        Arrays.parallelPrefix(dp, Math::multiplyExact);
         Stream.of(obstacleGrid).skip(1).forEach(row -> {
             dp[0] *= 1 - row[0];
-            IntStream.range(1, row.length).forEach(column -> dp[column] = IntStream.range(column - 1, column + 1).map($ -> dp[$]).sum() * (1 - row[column]));});
+            IntStream.range(1, row.length).forEach(column -> dp[column] = IntStream.range(column - 1, column + 1).map($ -> dp[$]).sum() * (1 - row[column]));
+            System.out.println(Arrays.toString(dp));});
         return dp[dp.length - 1];   
     }
 }
