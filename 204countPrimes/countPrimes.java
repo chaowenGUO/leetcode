e1658c5f99e29a2392188862c7edabb7231c20c3
chaowen guo <chaowen.guo1@gmail.com>
2020-01-13 19:41:10 -0600

    Create countPrimes.java

---
+++ 204countPrimes/countPrimes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n < 2) return 0;
+        else
+        {
+            List<Integer> result = Collections.nCopies(n, 1).stream().collect(Collectors.toList());
+            result.set(0, 0);
+            result.set(1, 0);
+            IntStream.range(2, (int)Math.sqrt(n) + 1).forEach(i -> {
+                for (int j = (int)Math.pow(i, 2); j < n; j += i) result.set(j, 0);});
+            return result.stream().mapToInt(Integer::intValue).sum();
+        }
+    }
+}
