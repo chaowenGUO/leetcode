6d8b8f00a7d61ec3e35eb6ea1b7319b1bdf0ec54
chaowen guo <chaowen.guo1@gmail.com>
2020-01-15 16:13:32 -0600

    Update generateParentheses.java

--- 22generateParentheses/generateParentheses.java
+++ 22generateParentheses/generateParentheses.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        final List<ArrayList<String>> dp = java.util.stream.IntStream.range(0, n + 1).mapToObj($ -> new ArrayList<String>()).collect(java.util.stream.Collectors.toList());
+        final List<List<String>> dp = IntStream.range(0, n + 1).mapToObj($ -> Collections.<String>emptyList().stream().collect(Collectors.toList())).collect(Collectors.toList());
         dp.get(0).add("");
-        java.util.stream.IntStream.range(0, n + 1).forEach(i -> java.util.stream.IntStream.range(0, i).forEach(j -> dp.get(i).addAll(dp.get(j).stream().flatMap(x -> dp.get(i - 1 - j).stream().map(y -> "(" + x + ")" + y)).collect(java.util.stream.Collectors.toList()))));
+        IntStream.range(0, n + 1).forEach(i -> IntStream.range(0, i).forEach(j -> dp.get(i).addAll(dp.get(j).stream().flatMap(x -> dp.get(i - j - 1).stream().map(y -> "(" + x + ")" + y)).collect(Collectors.toList()))));
         return dp.get(dp.size() - 1);
     }
 }
