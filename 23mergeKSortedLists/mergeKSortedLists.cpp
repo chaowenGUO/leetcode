dadda043cd363291cd3593baeac2825229037f97
chaowen guo <chaowen.guo1@gmail.com>
2020-01-15 21:17:24 -0600

    Update mergeKSortedLists.cpp

--- 23mergeKSortedLists/mergeKSortedLists.cpp
+++ 23mergeKSortedLists/mergeKSortedLists.cpp
@@ -9,21 +9,23 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        std::priority_queue<int, std::vector<int>, std::greater<int>> queue;
+        auto const compare{[](auto const a, auto const b){return a->val > b->val;}};
+        std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(compare)> queue{compare};
         for (auto&_: lists)
             while (_)
             {
-                queue.emplace(_->val);
+                queue.emplace(_);
                 _ = _->next;
             }
         auto const result{std::make_unique<ListNode>(0)};
         auto current{result.get()};
         while (!std::empty(queue))
         {
-            current->next = new ListNode{queue.top()};
+            current->next = queue.top();
             queue.pop();
             current = current->next;
         }
+        current->next = nullptr;
         return result->next;
     }
 };
