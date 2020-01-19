bf43957604411ab2408171f3946866fadeed8293
chaowen guo <chaowen.guo1@gmail.com>
2020-01-18 19:57:20 -0600

    Update decodeWays.java

--- 91decodeWays/decodeWays.java
+++ 91decodeWays/decodeWays.java
@@ -1,20 +1,19 @@
 class Solution {
     public int numDecodings(String s) {
-        List<String> ss = s.codePoints().mapToObj($ -> String.format("%c", $)).collect(Collectors.toList());
-        if (ss.get(0).equals("0")) return 0;
+        String[] ss = s.codePoints().mapToObj($ -> String.format("%c", $)).toArray(String[]::new);
+        if (ss[0].equals("0")) return 0;
         else
         {
-            int a = 1, b = 1, c = 0;
-            for (int $ = 1; $ != ss.size(); ++$)
+            LinkedList<Integer> result = IntStream.of(1, 1, 0).boxed().collect(Collectors.toCollection(LinkedList::new));
+            for (int $ = 1; $ != ss.length; ++$)
             {
-                if (!ss.get($).equals("0")) c = b;
-                final String subList = ss.subList($ - 1, $  + 1).stream().collect(Collectors.joining());
-                if (0 <= subList.compareTo("10") && subList.compareTo("26") <= 0) c += a;
-                a = b;
-                b = c;
-                c = 0;
+                if (!ss[$].equals("0")) result.set(result.size() - 1, result.get(1));
+                final String subList = ss[$ - 1] + ss [$];
+                if (0 <= subList.compareTo("10") && subList.compareTo("26") <= 0) result.set(result.size() - 1, result.get(2) + result.get(0));
+                result.poll();
+                result.offer(0);
             }
-            return b;
+            return result.get(1);
         }
     }
 }
