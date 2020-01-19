bc0d8f90f76cf9e7efb097728f7eaf8c3b3a5628
chaowen guo <chaowen.guo1@gmail.com>
2020-01-18 19:42:40 -0600

    Create regularExpressionMatching.cpp

---
+++ 10regularExpressionMatching/regularExpressionMatching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        std::vector<bool> dp(std::size(p) + 1);
+        dp.front() = true;
+        for (std::size_t column{2}; column < std::size(dp); ++column)
+            if (p.at(column - 1) == '*') dp.at(column) = dp.at(column - 2);
+        auto prev{dp};
+        for (std::size_t row{1}; row != std::size(s) + 1; ++row)
+        {
+            dp.front() = false;
+            for (std::size_t column{1}; column != std::size(dp); ++column)
+            {
+                if (s.at(row - 1) == p.at(column - 1) || p.at(column - 1) == '.') dp.at(column) = prev.at(column - 1);
+                else if (p.at(column - 1) == '*')
+                {
+                    if (p.at(column - 2) == s.at(row - 1) || p.at(column - 2) == '.') dp.at(column) = dp.at(column - 2) || prev.at(column);
+                    else dp.at(column) = dp.at(column - 2);
+                }
+                else dp.at(column) = false;
+            }
+            std::swap(prev, dp);
+        }
+        return prev.back();
+    }
+};
