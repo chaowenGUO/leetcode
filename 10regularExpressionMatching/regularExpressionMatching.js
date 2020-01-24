ce277c6b737bfaa039bb8e3017d38083be4219f2
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:13:14 -0600

    Update regularExpressionMatching.js

--- 10regularExpressionMatching/regularExpressionMatching.js
+++ 10regularExpressionMatching/regularExpressionMatching.js
@@ -6,11 +6,11 @@
 var isMatch = function(s, p) {
     s = [...s]
     p = [...p]
-    let dp = [true, ...Array(p.length).fill(false)]
+    const dp = [true, ...Array(p.length).fill(false)]
     for (let column = 2; column < dp.length; ++column) dp[column] = dp[column - 2] && Object.is(p[column - 1], '*')
     for (let row = 1; !Object.is(row, s.length + 1); ++row)
     {
-        let prev = [...dp]
+        const prev = [...dp]
         dp[0] = false
         for (let column = 1; !Object.is(column, dp.length); ++column)
         {
