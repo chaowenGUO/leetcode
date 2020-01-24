fb7ffea4b498446148a28f99b893d0b93632347c
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:19:27 -0600

    Update wildcardMatching.py

--- 44wildcardMatching/wildcardMatching.py
+++ 44wildcardMatching/wildcardMatching.py
@@ -1,14 +1,12 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         dp = [True, *itertools.repeat(False, len(p))]
-        for column in range(1, len(dp)):
-            if p[column - 1] == '*': dp[column] = dp[column - 1]
-        prev = dp[:]
+        for column in range(1, len(dp)): dp[column] = dp[column - 1] and p[column - 1] == '*'        
         for row in range(1, len(s) + 1):
+            prev = [*dp]
             dp[0] = False
             for column in range(1, len(dp)):
                 if s[row - 1] == p[column - 1] or p[column - 1] == '?': dp[column] = prev[column - 1]
-                elif p[column - 1] == '*': dp[column] = dp[column - 1] or prev[column]                   
+                elif p[column - 1] == '*': dp[column] = dp[column - 1] or prev[column]
                 else: dp[column] = False
-            prev, dp = dp, prev              
-        return prev[-1]
+        return dp[-1]
