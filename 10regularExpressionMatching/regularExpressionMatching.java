e99ca1c7d2c2918846b19f27e8db064877df59b3
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:16:00 -0600

    Update regularExpressionMatching.java

--- 10regularExpressionMatching/regularExpressionMatching.java
+++ 10regularExpressionMatching/regularExpressionMatching.java
@@ -2,26 +2,14 @@ class Solution {
     public boolean isMatch(String s, String p) {
         boolean[] dp = new boolean[p.codePointCount(0, p.length()) + 1];
         dp[0] = true;
-        for (int column = 2; column < dp.length; ++column)
-            if (p.codePointAt(column - 1) == "*".codePointAt(0)) dp[column] = dp[column - 2];
-        boolean[] prev = dp.clone();
-        for (int row = 1; row != s.codePointCount(0, s.length()) + 1; ++row)
-        {
+        IntStream.range(2, dp.length).forEach(column -> dp[column] = dp[column - 2] && p.codePointAt(column - 1) == "*".codePointAt(0));
+        IntStream.range(1, s.codePointCount(0, s.length()) + 1).forEach(row -> {
+            final boolean[] prev = dp.clone();
             dp[0] = false;
-            for (int column = 1; column != dp.length; ++column)
-            {
+            IntStream.range(1, dp.length).forEach(column -> {
                 if (s.codePointAt(row - 1) == p.codePointAt(column - 1) || p.codePointAt(column - 1) == ".".codePointAt(0)) dp[column] = prev[column - 1];
-                else if (p.codePointAt(column - 1) == "*".codePointAt(0))
-                {
-                    if (p.codePointAt(column - 2) == s.codePointAt(row - 1) || p.codePointAt(column - 2) == ".".codePointAt(0)) dp[column] = dp[column - 2] || prev[column];
-                    else dp[column] = dp[column - 2];
-                }
-                else dp[column] = false;
-            }
-            boolean[] tmp = prev;
-            prev = dp;
-            dp = tmp;
-        }
-        return prev[prev.length - 1];
+                else if (p.codePointAt(column - 1) == "*".codePointAt(0)) dp[column] = dp[column - 2] || prev[column] && (p.codePointAt(column - 2) == s.codePointAt(row - 1) || p.codePointAt(column - 2) == ".".codePointAt(0));
+                else dp[column] = false;});});
+        return dp[dp.length - 1];
     }
 }
