865717278fbf9fe39209e0342a3b790d29f80656
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:11:54 -0600

    Update regularExpressionMatching.py

--- 10regularExpressionMatching/regularExpressionMatching.py
+++ 10regularExpressionMatching/regularExpressionMatching.py
@@ -2,12 +2,11 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         dp = [True, *itertools.repeat(False, len(p))]
         for column in range(2, len(dp)): dp[column] = dp[column - 2] and p[column - 1] == '*'
-        prev = [*dp]
         for row in range(1, len(s) + 1):
+            prev = [*dp]
             dp[0] = False
             for column in range(1, len(dp)):
                 if s[row - 1] == p[column - 1] or p[column - 1] == '.': dp[column] = prev[column - 1]
                 elif p[column - 1] == '*': dp[column] = dp[column - 2] or prev[column] and (s[row - 1] == p[column - 2] or p[column - 2] == '.')
                 else: dp[column] = False
-            prev, dp = dp, prev
-        return prev[-1]
+        return dp[-1]
