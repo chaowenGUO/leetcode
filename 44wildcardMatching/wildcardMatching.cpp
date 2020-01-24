000656bbc2401d9c3e9b5a2adc332cd7f6e25ea9
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:18:33 -0600

    Update wildcardMatching.cpp

--- 44wildcardMatching/wildcardMatching.cpp
+++ 44wildcardMatching/wildcardMatching.cpp
@@ -3,11 +3,10 @@ public:
     bool isMatch(string s, string p) {
         std::vector<bool> dp(std::size(p) + 1);
         dp.front() = true;
-        for (std::size_t column{1}; column != std::size(dp); ++column)
-            if (p.at(column - 1) == '*') dp.at(column) = dp.at(column - 1);
-        auto prev{dp};
+        for (std::size_t column{1}; column != std::size(dp); ++column) dp.at(column) = dp.at(column - 1) && p.at(column - 1) == '*';
         for (std::size_t row{1}; row != std::size(s) + 1; ++row)
         {
+            auto const prev{dp};
             dp.front() = false;
             for (std::size_t column{1}; column != std::size(dp); ++column)
             {
@@ -15,8 +14,7 @@ public:
                 else if (p.at(column - 1) == '*') dp.at(column) = dp.at(column - 1) || prev.at(column);
                 else dp.at(column) = false;
             }
-            std::swap(prev, dp);
         }
-        return prev.back();
+        return dp.back();
     }
 };
