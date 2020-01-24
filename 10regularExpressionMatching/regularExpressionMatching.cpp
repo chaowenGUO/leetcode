5dac5a94d0ff5ebef142feb6ef0b3e31ddf2e19c
chaowen guo <chaowen.guo1@gmail.com>
2020-01-23 18:14:11 -0600

    Update regularExpressionMatching.cpp

--- 10regularExpressionMatching/regularExpressionMatching.cpp
+++ 10regularExpressionMatching/regularExpressionMatching.cpp
@@ -3,24 +3,18 @@ public:
     bool isMatch(string s, string p) {
         std::vector<bool> dp(std::size(p) + 1);
         dp.front() = true;
-        for (std::size_t column{2}; column < std::size(dp); ++column)
-            if (p.at(column - 1) == '*') dp.at(column) = dp.at(column - 2);
-        auto prev{dp};
+        for (std::size_t column{2}; column < std::size(dp); ++column) dp.at(column) = dp.at(column - 2) && p.at(column - 1) == '*';
         for (std::size_t row{1}; row != std::size(s) + 1; ++row)
         {
+            auto prev{dp};
             dp.front() = false;
             for (std::size_t column{1}; column != std::size(dp); ++column)
             {
                 if (s.at(row - 1) == p.at(column - 1) || p.at(column - 1) == '.') dp.at(column) = prev.at(column - 1);
-                else if (p.at(column - 1) == '*')
-                {
-                    if (p.at(column - 2) == s.at(row - 1) || p.at(column - 2) == '.') dp.at(column) = dp.at(column - 2) || prev.at(column);
-                    else dp.at(column) = dp.at(column - 2);
-                }
+                else if (p.at(column - 1) == '*') dp.at(column) = dp.at(column - 2) || prev.at(column) && (p.at(column - 2) == s.at(row - 1) || p.at(column - 2) == '.');
                 else dp.at(column) = false;
             }
-            std::swap(prev, dp);
         }
-        return prev.back();
+        return dp.back();
     }
 };
