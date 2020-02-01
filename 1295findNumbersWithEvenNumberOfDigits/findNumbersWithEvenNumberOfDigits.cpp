d438b1723fe049e3cce8d51d56632c0cecd7117d
chaowen guo <chaowen.guo1@gmail.com>
2020-01-31 22:56:00 -0600

    Update findNumbersWithEvenNumberOfDigits.cpp

--- 1295findNumbersWithEvenNumberOfDigits/findNumbersWithEvenNumberOfDigits.cpp
+++ 1295findNumbersWithEvenNumberOfDigits/findNumbersWithEvenNumberOfDigits.cpp
@@ -1,17 +1,6 @@
 class Solution {
-private:
-    static constexpr auto even(int num)
-    {
-        auto result{0};
-        while (num)
-        {
-            num /= 10;
-            ++result;
-        }
-        return 1 - result & 1;
-    }
 public:
     int findNumbers(vector<int>& nums) {
-        return std::accumulate(std::begin(nums), std::transform(std::cbegin(nums), std::cend(nums), std::begin(nums), this->even), 0);
+        return std::count(std::begin(nums), std::transform(std::cbegin(nums), std::cend(nums), std::begin(nums), [](auto const num){return 1 - std::size(std::to_string(num)) & 1;}), 1);
     }
 };
