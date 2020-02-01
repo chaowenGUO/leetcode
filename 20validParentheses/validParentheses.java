b07af03ea2a144ca9ce760abb711c9fda299bea4
chaowen guo <chaowen.guo1@gmail.com>
2020-01-31 20:49:02 -0600

    Update validParentheses.java

--- 20validParentheses/validParentheses.java
+++ 20validParentheses/validParentheses.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-        final HashMap<String, String> parentheses = new HashMap<>();
-        parentheses.putIfAbsent("(", ")");
-        parentheses.putIfAbsent("[", "]");
-        parentheses.putIfAbsent("{", "}");
-        final LinkedList<String> stack = new LinkedList<>();
-        for (final String $: s.codePoints().mapToObj($ -> String.format("%c", $)).collect(Collectors.toList()))
+        final var parentheses = Map.ofEntries(Map.entry("{", "}"), Map.entry("[", "]"), Map.entry("(", ")"));
+        final var stack = new LinkedList<String>();
+        for (final var $: s.codePoints().mapToObj($ -> String.format("%c", $)).collect(Collectors.toList()))
+        {
             if (parentheses.containsKey($)) stack.push(parentheses.get($));
             else if (stack.isEmpty() || !stack.pop().equals($)) return false;
+        }
         return stack.isEmpty();
     }
 }
