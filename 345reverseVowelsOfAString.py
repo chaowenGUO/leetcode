e8f1734e28c1f39d5f863aa0ef5087f26f0b3e24
chaowen guo <chaowen.guo1@gmail.com>
2020-01-31 18:03:13 -0600

    Update 345reverseVowelsOfAString.py

--- 345reverseVowelsOfAString.py
+++ 345reverseVowelsOfAString.py
@@ -1,13 +1,4 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        *s, = s
-        left, right = 0, len(s) - 1
-        vowels = 'aeiouAEIOU'
-        while left < right:
-            if s[left] not in vowels: left += 1
-            elif s[right] not in vowels: right -= 1
-            else:
-                s[left], s[right] = s[right], s[left]
-                left += 1
-                right -= 1
-        return ''.join(s)
+        vowels = [group[0] for group in re.finditer('[aeiou]', s, re.I)]
+        return re.sub('[aeiou]', lambda group: vowels.pop(), s, 0, re.I)
