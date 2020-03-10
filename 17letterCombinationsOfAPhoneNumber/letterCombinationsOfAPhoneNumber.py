9957ebe41da68a519e0d20e825cc6b26cf2d40d2
chaowen guo <chaowen.guo1@gmail.com>
2020-03-09 23:03:00 -0500

    Update letterCombinationsOfAPhoneNumber.py

--- 17letterCombinationsOfAPhoneNumber/letterCombinationsOfAPhoneNumber.py
+++ 17letterCombinationsOfAPhoneNumber/letterCombinationsOfAPhoneNumber.py
@@ -1,6 +1,4 @@
-import functools
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         phone = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
-        return functools.reduce(lambda result, digit: [''.join(_) for _ in itertools.product(result, phone.get(int(digit)))], digits, ['']) if digits else []
+        return [''.join(_) for _ in itertools.product(*(phone.get(int(_)) for _ in digits))] if digits else []
