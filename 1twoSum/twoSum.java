6c9932ef2e884b9b675dba885a2319ffda84329d
chaowen guo <chaowen.guo1@gmail.com>
2020-03-28 01:56:19 -0500

    Update twoSum.java

--- 1twoSum/twoSum.java
+++ 1twoSum/twoSum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        final HashMap<Integer, Integer> dictionary = new HashMap<>();
-        final ListIterator<Integer> iter = Arrays.stream(nums).boxed().collect(Collectors.toList()).listIterator();
+        final var dictionary = new HashMap<Integer, Integer>();
+        final var iter = Arrays.stream(nums).boxed().collect(Collectors.toList()).listIterator();
         while (iter.hasNext())
         {
-            final int index = iter.nextIndex(), num = iter.next();
+            final var index = iter.nextIndex();
+            final var num = iter.next();
             if (dictionary.containsKey(num)) return new int[]{index, dictionary.get(num)};
             else dictionary.putIfAbsent(target - num, index);
         }
