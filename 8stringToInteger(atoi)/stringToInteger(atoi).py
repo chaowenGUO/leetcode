9414b272e5e02dd00447b992602088e68bfcd107
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 05:14:24 -0500

    Update stringToInteger(atoi).py

--- 8stringToInteger(atoi)/stringToInteger(atoi).py
+++ 8stringToInteger(atoi)/stringToInteger(atoi).py
@@ -2,4 +2,4 @@ class Solution:
     def myAtoi(self, str: str) -> int:
         result = re.search('^[+-]?\d+', str.strip())
         result = int(result[0] if result else 0)
-        return sorted((result, -1 << 31, (1 << 31) - 1))[1]
+        return statistics.median((-1 << 31, (1 << 31) - 1, result))
