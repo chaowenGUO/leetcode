d16815c5a9712968d97fc0cbbf4845e1311f4f39
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 05:28:44 -0500

    Update palindromeNumber.java

--- 9palindromeNumber/palindromeNumber.java
+++ 9palindromeNumber/palindromeNumber.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || x % 10 == 0 && x != 0) return false;
+        if (x < 0 || x != 0 && x % 10 == 0) return false;
         else
         {
-            int result = 0;
+            var result = 0;
             while (result < x)
             {
                 result = result * 10 + x % 10;
