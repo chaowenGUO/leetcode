858920148a1a9b6389300eff1ab05000a2ae4d1a
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 06:55:51 -0500

    Update threeSum.js

--- 15threeSum/threeSum.js
+++ 15threeSum/threeSum.js
@@ -3,22 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    nums.sort((a,b) => a - b)
     const result = []
-    nums.sort((a, b) => a - b)
     for (let index = 0; index < nums.length - 2;)
     {
         const num = nums[index]
         if (num > 0) break
-        let [left, right] = [index + 1, nums.length - 1]
-        while (left < right)
+        let left = index + 1, right = nums.length - 1
+        while (!Object.is(left, right))
         {
-            const [numsLeft, numsRight] = [left, right].map(_ => nums[_])
+            const numsLeft = nums[left], numsRight = nums[right]
             const total = num + numsLeft + numsRight
             if (Object.is(total, 0)) result.push([num, numsLeft, numsRight])
-            if (total <= 0) while (left < right && Object.is(numsLeft, nums[left])) ++left
-            if (total >= 0) while (left < right && Object.is(numsRight, nums[right])) --right
+            if (total <= 0) while (!Object.is(left, right) && Object.is(numsLeft, nums[left])) ++left
+            if (total >= 0) while (!Object.is(left, right) && Object.is(numsRight, nums[right])) --right
         }
-        while (Object.is(num, nums[index]) && index < nums.length - 2) ++index
+        while (index < nums.length - 2 && Object.is(nums[index], num)) ++index
     }
     return result
 };
