c00fa1afc0a1dbecbba1e5ccf7d877877546c113
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 09:11:50 -0500

    Update pow(x, n).py

--- 50pow(x, n)/pow(x, n).py
+++ 50pow(x, n)/pow(x, n).py
@@ -1,9 +1,11 @@
+import numpy
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         result = 1
-        sign = math.copysign(1, n)
+        sign = numpy.sign(n)
         while n:
             if n & 1: result *= x
             x *= x
             n = int(n / 2)
-        return result if sign == 1 else 1 / result
+        return result if sign == 1 else 1 / result 
