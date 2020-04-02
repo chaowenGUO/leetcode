787b26b4d82881d0f95df4d35e3384dbde0e4ac6
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 06:27:49 -0500

    Update romanToInteger.java

--- 13romanToInteger/romanToInteger.java
+++ 13romanToInteger/romanToInteger.java
@@ -1,6 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
-        final var Roman = Map.ofEntries(Map.entry("M", 1000), Map.entry("D", 500), Map.entry("C", 100), Map.entry("L", 50), Map.entry("X", 10), Map.entry("V", 5), Map.entry("I", 1));
-        return new StringBuilder(s).reverse().toString().codePoints().mapToObj($ -> String.format("%c", $)).reduce(Map.entry(0, "I"), (result, $) -> Map.entry(Roman.get($) < Roman.get(result.getValue()) ? result.getKey() - Roman.get($) : result.getKey() + Roman.get($), $), (x, y) -> x).getKey();
+        final var roman = Collections.unmodifiableMap(Map.ofEntries(Map.entry("M", 1000), Map.entry("D", 500), Map.entry("C", 100), Map.entry("L", 50), Map.entry("X", 10), Map.entry("V", 5), Map.entry("I", 1)));
+        return new StringBuilder(s).reverse().toString().codePoints().mapToObj($ -> String.format("%c", $)).reduce(Map.entry(0, "I"), (result, $) -> Map.entry(roman.get($) < roman.get(result.getValue()) ? result.getKey() - roman.get($) : result.getKey() + roman.get($) ,$), (x, y) -> x).getKey();
     }
 }
