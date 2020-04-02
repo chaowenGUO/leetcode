f8e8f24332d49519468e6ac6bd66d25ce73a8dd8
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 04:38:52 -0500

    Update 6zigZagConversion.py

--- 6zigZagConversion.py
+++ 6zigZagConversion.py
@@ -4,8 +4,8 @@ class Solution:
         else:
             result = [''] * numRows
             row, direction = 0, 1
-            for char in s:
-                result[row] += char
+            for _ in s:
+                result[row] += _
                 if row == 0: direction = 1
                 elif row == numRows - 1: direction = -1
                 row += direction
