f41eaaae77c950c0cbf65133ea9a2f829145dbc3
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 05:12:19 -0500

    Update reverseInteger.java

--- 7reverseInteger/reverseInteger.java
+++ 7reverseInteger/reverseInteger.java
@@ -1,6 +1,6 @@
 class Solution {
     public int reverse(int x) {
-        int result = 0;
+        var result = 0;
         while (x != 0)
             if (Math.abs(result) > Integer.MAX_VALUE / 10) return 0;
             else
