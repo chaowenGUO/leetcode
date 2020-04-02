6e0c5ed4613ad92ba7589d556b90ee80ec816cba
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 07:29:17 -0500

    Update threeSumClosest.java

--- 16threeSumClosest/threeSumClosest.java
+++ 16threeSumClosest/threeSumClosest.java
@@ -1,15 +1,17 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int result = Arrays.stream(nums).limit(3).sum();
-        for (int index = 0; index != nums.length - 2;)
+        var result = Arrays.stream(nums).limit(3).sum();
+        for (var index = 0; index != nums.length - 2;)
         {
-            final int num = nums[index];
-            int left = index + 1, right = nums.length - 1;
+            final var num = nums[index];
+            var left = index + 1;
+            var right = nums.length - 1;
             while (left != right)
             {
-                final int numsLeft = nums[left], numsRight = nums[right];
-                final int total = num + numsLeft + numsRight;
+                final var numsLeft = nums[left];
+                final var numsRight = nums[right];
+                final var total = num + numsLeft + numsRight;
                 if (total == target) return target;
                 if (total <= target) while (left != right && numsLeft == nums[left]) ++left;
                 if (total >= target) while (left != right && numsRight == nums[right]) --right;
