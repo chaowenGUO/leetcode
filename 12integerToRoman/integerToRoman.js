87cd59a558199601136c7978e1e2c85616690d2e
chaowen guo <chaowen.guo1@gmail.com>
2020-04-02 05:55:32 -0500

    Update integerToRoman.js

--- 12integerToRoman/integerToRoman.js
+++ 12integerToRoman/integerToRoman.js
@@ -5,8 +5,8 @@
 var intToRoman = function(num) {
     const Roman = Object.freeze([['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]])
     const result = []
-    Roman.forEach(([symbol,value]) => {
-        result.push(symbol.repeat(num / value))
+    Roman.forEach(([name,value]) => {
+        result.push(name.repeat(num / value))
         num %= value})
     return result.join('')
 };
