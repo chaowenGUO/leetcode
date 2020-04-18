79dae754c3ec2ba328ab1101d0d47b8100db8f0b
chaowen guo <chaowen.guo1@gmail.com>
2020-04-18 00:06:44 -0500

    Update removeElement.cpp

--- 27removeElement/removeElement.cpp
+++ 27removeElement/removeElement.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        std::size_t index{0};
-        std::for_each(std::cbegin(nums), std::cend(nums), [&](auto const num){
-            if (num != val)
-            {
-                nums.at(index) = num;
-                ++index;}});
-        return index;
+        return std::distance(std::begin(nums), std::remove(std::begin(nums), std::end(nums), val));
     }
 };
