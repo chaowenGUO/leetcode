28ee0b6f39cce9cef5bca7bf3526bbfcddcb63de
chaowen guo <chaowen.guo1@gmail.com>
2020-04-17 22:25:39 -0500

    Update generateParentheses.py

--- 22generateParentheses/generateParentheses.py
+++ 22generateParentheses/generateParentheses.py
@@ -2,6 +2,5 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         dp = [[''], *([] for _ in range(n))]
         for i in range(n + 1):
-            for j in range(i):
-                dp[i] += (f'({x}){y}' for ｘ in dp[j] for y in dp[i - j - 1])
+            for j in range(i): dp[i] += (f'({x}){y}' for x, y in itertools.product(dp[j], dp[i - j - 1]))
         return dp[-1]
