534137fdbe3529932d10ead54e409ed19a3ef2fc
chaowen guo <chaowen.guo1@gmail.com>
2020-04-17 23:58:57 -0500

    Update removeDuplicatesFromSortedArray.py

--- 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
+++ 26removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.py
@@ -2,7 +2,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         index = 1 if nums else 0
         for num in nums:
-            if num > nums[index - 1]:
+            if num != nums[index - 1]:
                 nums[index] = num
                 index += 1
         return index
