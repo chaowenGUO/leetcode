cb205c5bf8a4f3995ee43704567c8a115ec22997
chaowen guo <chaowen.guo1@gmail.com>
2020-04-17 20:52:56 -0500

    Update threeSumClosest.js

--- 16threeSumClosest/threeSumClosest.js
+++ 16threeSumClosest/threeSumClosest.js
@@ -3,23 +3,26 @@
  * @param {number} target
  * @return {number}
  */
+
+const lodash = require('lodash')
+
 var threeSumClosest = function(nums, target) {
-    nums.sort((a,b) => a - b)
-    let result = nums.slice(0, 3).reduce((a, b) => a + b)
+    nums.sort((a, b) => a - b)
+    let result = lodash.chain(nums).slice(0, 3).sum()
     for (let index = 0; !Object.is(index, nums.length - 2);)
     {
         const num = nums[index]
-        let [left, right] = [index + 1, nums.length - 1]
+        let left = index + 1, right = nums.length - 1
         while (!Object.is(left, right))
         {
-            const [numsLeft, numsRight] = [left, right].map(_ => nums[_])
+            const numsLeft = nums[left], numsRight = nums[right]
             const total = num + numsLeft + numsRight
             if (Object.is(total, target)) return target
-            if (total <= target) while (!Object.is(left, right) && Object.is(nums[left], numsLeft)) ++left
-            if (total >= target) while (!Object.is(left, right) && Object.is(nums[right], numsRight)) --right
+            if (total <= target) while (!Object.is(left, right) && Object.is(numsLeft, nums[left])) ++left
+            if (total >= target) while (!Object.is(left, right) && Object.is(numsRight, nums[right])) --right
             if (Math.abs(total - target) < Math.abs(result - target)) result = total
         }
-        while (Object.is(nums[index], num) && !Object.is(index, nums.length - 2)) ++index
+        while (!Object.is(index, nums.length - 2) && Object.is(nums[index], num)) ++index
     }
     return result
 };
