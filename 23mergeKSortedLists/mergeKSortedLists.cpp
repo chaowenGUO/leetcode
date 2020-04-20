f49d5b6644f94811448744109ca77e1c913ac313
chaowen guo <chaowen.guo1@gmail.com>
2020-04-19 23:05:41 -0500

    Update mergeKSortedLists.cpp

--- 23mergeKSortedLists/mergeKSortedLists.cpp
+++ 23mergeKSortedLists/mergeKSortedLists.cpp
@@ -10,24 +10,24 @@ class Solution {
 private:
     auto list(ListNode* list)
     {
-        std::forward_list<decltype(std::declval<ListNode>().val)> result;
+        std::forward_list<ListNode*> result;
         auto position{result.cbefore_begin()};
         while (list)
         {
-            position = result.emplace_after(position, list->val);
+            position = result.emplace_after(position, list);
             list = list->next;
         }
         return result;
     }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        std::forward_list<decltype(std::declval<ListNode>().val)> list;
-        for (auto const&_: lists) list.merge(this->list(_));
+        std::forward_list<ListNode*> list;
+        for (auto const&_: lists) list.merge(this->list(_), [](auto const a, auto const b){return a->val < b->val;});
         auto const result{std::make_unique<ListNode>(0)};
         auto current{result.get()};
         for (auto const _: list)
         {
-            current->next = new ListNode{_};
+            current->next = _;
             current = current->next;
         }
         return result->next;
